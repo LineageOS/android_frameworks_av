ddafd732bf18d6a3370da1a3354a720ef460feed
Bin Chen <chen_bin@projectara.com>
2016-02-22 18:19:58 +1100

    CameraService: Fix cameraId type of onDeviceStatusChanged
    The cameraId should be of type int, instead of camera_device_status_t.
    
    Change-Id: I26d587bb74f7100028f09928984c2e8dad6eebef
    Signed-off-by: Bin Chen <chen_bin@projectara.com>

--- services/camera/libcameraservice/CameraService.h
+++ services/camera/libcameraservice/CameraService.h
@@ -99,7 +99,7 @@ public:
 
     /////////////////////////////////////////////////////////////////////
     // HAL Callbacks
-    virtual void        onDeviceStatusChanged(camera_device_status_t cameraId,
+    virtual void        onDeviceStatusChanged(int cameraId,
                                               camera_device_status_t newStatus);
     virtual void        onTorchStatusChanged(const String8& cameraId,
                                              int32_t newStatus);
