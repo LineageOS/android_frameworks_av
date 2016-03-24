e15b5678f81b4fb845b89878692d4001ede70587
Dan Austin <danielaustin@google.com>
2016-03-24 12:21:43 -0700

    Address const issues in preparation for libcxx rebase.
    Change-Id: I61777e557738b2df8b6037a6f71a2cf0a24967dd

--- media/libstagefright/httplive/M3UParser.cpp
+++ media/libstagefright/httplive/M3UParser.cpp
@@ -498,8 +498,8 @@ static bool MakeURL(const char *baseURL, const char *url, AString *out) {
     if (url[0] == '/') {
         // URL is an absolute path.
 
-        char *protocolEnd = strstr(baseURL, "//") + 2;
-        char *pathStart = strchr(protocolEnd, '/');
+        const char *protocolEnd = strstr(baseURL, "//") + 2;
+        const char *pathStart = strchr(protocolEnd, '/');
 
         if (pathStart != NULL) {
             out->setTo(baseURL, pathStart - baseURL);
