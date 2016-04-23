88e49d57a13e04982412b21cae090ae50ca59085
Steve Kondik <steve@cyngn.com>
2016-04-22 18:32:39 -0700

    audiopolicy: Add AudioSessionInfo API
     * This patch introduces a new API which allows applications to
       query the state of the audio effects system, and receive
       callbacks with the necessary information to attach effects
       to any stream.
     * In the future, this may come as part of the AudioPort system,
       but since that's an active area of development by Google, we
       will dodge it for now.
     * The policy now simply keeps a refcounted list of objects which
       hold various bits of stream metadata. Callbacks are sent on
       stream open/close to applications which might be listening
       for them.
    
    Change-Id: I2d554d36e1378f4eb7b276010a3bfe8345c22ecd
    
    audiopolicy: Constrain session events to music streams
    
     * We're really only interested in music streams right now, but
       events are being generated for all streams (system sounds, etc).
     * Constrain for now, in the future we will filter based on client
       registrations.
    
    Change-Id: Ic445052028c454eed146addebcdb28c4b26c4f20
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    audiopolicy: Use audio_session_t consistently for AudioSessionInfo
    
    In N, the usage of audio_session_t was made more consistent in the
    commit d848eb48c121c119e8ba7583efc75415fe102570.
    
    Do the same for the AudioSessionInfo API.
    
    Change-Id: I8732225ec6788777e7be9ed42f2ca4002af47c3b
    
    [mikeioannina]: Update for Oreo
    [bgcngm]: Update for Pie
    [sam3000]: Update for 10
    
    Change-Id: I2d554d36e1378f4eb7b276010a3bfe8345c22ecd

--- media/libaudioclient/include/media/IAudioPolicyService.h
+++ media/libaudioclient/include/media/IAudioPolicyService.h
@@ -222,6 +222,9 @@ public:
                                                        volume_group_t &volumeGroup) = 0;
 
     virtual status_t setRttEnabled(bool enabled) = 0;
+
+    virtual status_t listAudioSessions(audio_stream_type_t streams,
+                                       Vector< sp<AudioSessionInfo>> &sessions) = 0;
 };
 
 
