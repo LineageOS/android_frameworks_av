24f7b559abab11a1a02c49c5c4069564ed32ead9
Tom Marshall <tdm@cyngn.com>
2016-06-02 10:35:32 -0700

    MTP: Fix crash when no storages are available
    Change-Id: I6d7202ade46a5d781a3db5a1a3bdde17c8e70a60
    
    MTP: Return nullptr, not NULL
    
    Change-Id: I6dcd4d6ea10661917d9d9b0ae62d85000db89c35

--- media/mtp/MtpServer.cpp
+++ media/mtp/MtpServer.cpp
@@ -158,7 +158,7 @@ void MtpServer::removeStorage(MtpStorage* storage) {
 
 MtpStorage* MtpServer::getStorage(MtpStorageID id) {
     if (id == 0)
-        return mStorages[0];
+        return mStorages.empty() ? nullptr : mStorages[0];
     for (size_t i = 0; i < mStorages.size(); i++) {
         MtpStorage* storage = mStorages[i];
         if (storage->getStorageID() == id)
