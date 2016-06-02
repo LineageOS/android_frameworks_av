caaf5eefef06dc534a77fa0cbb07e8b37c2dee9c
Glenn Kasten <gkasten@google.com>
2016-06-02 12:15:19 -0700

    Build libcpustats for host
    Test: builds OK; host program that uses this is still WIP
    Change-Id: I9427cfadb102ab3379ecc0f9209a4f0660a838cd

--- media/libcpustats/Android.mk
+++ media/libcpustats/Android.mk
@@ -11,3 +11,15 @@ LOCAL_MODULE := libcpustats
 LOCAL_CFLAGS := -Werror -Wall
 
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES :=     \
+        CentralTendencyStatistics.cpp \
+        ThreadCpuUsage.cpp
+
+LOCAL_MODULE := libcpustats
+
+LOCAL_CFLAGS := -std=gnu++11 -Werror -Wall
+
+include $(BUILD_HOST_STATIC_LIBRARY)
