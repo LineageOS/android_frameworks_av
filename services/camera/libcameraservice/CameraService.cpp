415f90a3701382f0ea263182ec9afa9257e25087
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2016-07-05 12:03:38 +0530

    CameraService: Fix deadlock in binder death cleanup.
    Issue:
    In the event of a binder death, there is a chance of deadlock
    due to recursive lock acquisition in the death handling sequence.
    
    Fix:
    Clear evicted client list before acquiring service lock.
    
    CRs-Fixed: 984249
    Change-Id: I6fc5fa6e01c002bc46be058fcd977be14cae0270

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1781,7 +1781,8 @@ bool CameraService::evictClientIdByRemote(const wp<IBinder>& remote) {
                 ret = true;
             }
         }
-
+        //clear the evicted client list before acquring service lock again.
+        evicted.clear();
         // Reacquire mServiceLock
         mServiceLock.lock();
 
