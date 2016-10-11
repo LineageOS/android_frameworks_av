7714cd599867d134ef15904adf6215e751d1f24c
Marco Nelissen <marcone@google.com>
2016-10-11 09:24:54 -0700

    Add missing extensions, fix scan of bad media files.
    Update extensions list in StagefrightMediaScanner.
    Mark files that fail to scan as non-media files.
    
    Bug: 31900187
    Bug: 32045229
    Bug: 77656952
    Bug: 78682917
    Change-Id: I78f21b044f3a038d6e78dbe9f461f650650afe8b
    (cherry picked from commit 9abe5ee514114a58728eed08253f8d08bd90b959)

--- media/libstagefright/StagefrightMediaScanner.cpp
+++ media/libstagefright/StagefrightMediaScanner.cpp
@@ -40,7 +40,8 @@ static bool FileHasAcceptableExtension(const char *extension) {
         ".mpeg", ".ogg", ".mid", ".smf", ".imy", ".wma", ".aac",
         ".wav", ".amr", ".midi", ".xmf", ".rtttl", ".rtx", ".ota",
         ".mkv", ".mka", ".webm", ".ts", ".fl", ".flac", ".mxmf",
-        ".avi", ".mpeg", ".mpg", ".awb", ".mpga", ".mov", ".opus"
+        ".avi", ".mpeg", ".mpg", ".awb", ".mpga", ".mov", ".opus",
+        ".m4v", ".oga"
     };
     static const size_t kNumValidExtensions =
         sizeof(kValidExtensions) / sizeof(kValidExtensions[0]);
@@ -62,6 +63,11 @@ MediaScanResult StagefrightMediaScanner::processFile(
     client.setLocale(locale());
     client.beginFile();
     MediaScanResult result = processFileInternal(path, mimeType, client);
+    ALOGV("result: %d", result);
+    if (mimeType == NULL && result != MEDIA_SCAN_RESULT_OK) {
+        ALOGW("media scan failed for %s", path);
+        client.setMimeType("application/octet-stream");
+    }
     client.endFile();
     return result;
 }
