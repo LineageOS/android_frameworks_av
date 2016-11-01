e813ef9388e9f0b821c0a434521f0e194288c8ad
Kiran Kumar Krishna <kiran@motorola.com>
2016-11-01 07:50:22 -0500

    AudioRecord: fix AudioRecordThread crash upon exit
    In case of error during openRecord_l(), there is a possibility
    that a newly created AudioRecordThread is resumed by requestExit()
    just after entering the Thread class _threadLoop() but before
    entering the class threadLoop(). In this case, processAudioBuffer()
    is executed once with mCblk == 0 and we assert.
    
    Test: test case at bug
    Bug: 29993798
    Change-Id: I15d4f138218f6f01a020c4fc568a5b4831cb92a7

--- media/libaudioclient/AudioRecord.cpp
+++ media/libaudioclient/AudioRecord.cpp
@@ -1274,6 +1274,9 @@ bool AudioRecord::AudioRecordThread::threadLoop()
             return true;
         }
     }
+    if (exitPending()) {
+        return false;
+    }
     nsecs_t ns =  mReceiver.processAudioBuffer();
     switch (ns) {
     case 0:
