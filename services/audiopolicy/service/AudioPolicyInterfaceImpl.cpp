1f33f250d5ecdaa9a674a7bac6e8feb8467b9aed
myfluxi <linflux@arcor.de>
2016-11-22 23:11:06 +0100

    audioflinger: Fix audio for WifiDisplay
    AudioFlinger is not able to determine the correct
    pid/tid for WifiDisplay and thus we do not pass checks
    for CAPTURE_AUDIO_OUTPUT and RECORD_AUDIO permissions.
    
    To fix audio for WifiDisplay, it should be safe to
    always allow a trusted calling uid (AID_MEDIA which
    has the same perms as AID_AUDIOSERVER).
    
    Change-Id: Ifa46d8e77a43027645cad02a04263b58e134c3ad

--- services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
+++ services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
@@ -396,7 +396,7 @@ status_t AudioPolicyService::getInputForAttr(const audio_attributes_t *attr,
     }
 
     // check calling permissions
-    if (!recordingAllowed(opPackageName, pid, uid)) {
+    if (!isAudioServerOrMediaServerUid(callingUid) && !recordingAllowed(opPackageName, pid, uid)) {
         ALOGE("%s permission denied: recording not allowed for uid %d pid %d",
                 __func__, uid, pid);
         return PERMISSION_DENIED;
@@ -443,7 +443,7 @@ status_t AudioPolicyService::getInputForAttr(const audio_attributes_t *attr,
             case AudioPolicyInterface::API_INPUT_TELEPHONY_RX:
                 // FIXME: use the same permission as for remote submix for now.
             case AudioPolicyInterface::API_INPUT_MIX_CAPTURE:
-                if (!canCaptureOutput) {
+                if (!isAudioServerOrMediaServerUid(callingUid) && !canCaptureOutput) {
                     ALOGE("getInputForAttr() permission denied: capture not allowed");
                     status = PERMISSION_DENIED;
                 }
@@ -512,7 +512,7 @@ status_t AudioPolicyService::startInput(audio_port_handle_t portId)
     }
 
     // check calling permissions
-    if (!startRecording(client->opPackageName, client->pid, client->uid)) {
+    if (!isAudioServerOrMediaServerUid(IPCThreadState::self()->getCallingUid()) && !startRecording(client->opPackageName, client->pid, client->uid)) {
         ALOGE("%s permission denied: recording not allowed for uid %d pid %d",
                 __func__, client->uid, client->pid);
         return PERMISSION_DENIED;
