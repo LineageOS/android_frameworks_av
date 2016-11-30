16bae20c76c614090c094c6898a845bc23fd4faa
Sharad Sangle <assangle@codeaurora.org>
2016-11-30 17:21:06 +0530

    audio: Don't update the audio_output flag with track flags
     Use the AUDIO_OUTPUT_FLAG_DIRECT only for creating
     the track and do not add this in the audio_flags
     of AudioTrack, as adding this to audio_flags of
     AudioTrack is not expected and lead to side effects.
    
    Change-Id: Ic85ad5aef91e147a209d1c7a3a7ef0728f3d4207

--- media/libmedia/AudioTrack.cpp
+++ media/libmedia/AudioTrack.cpp
@@ -1402,6 +1402,7 @@ status_t AudioTrack::createTrack_l()
     }
 
     audio_output_flags_t flags = mFlags;
+    audio_output_flags_t trackFlags = mFlags;
 
     pid_t tid = -1;
     if (mFlags & AUDIO_OUTPUT_FLAG_FAST) {
@@ -1411,7 +1412,7 @@ status_t AudioTrack::createTrack_l()
     }
 
     if (mTrackOffloaded) {
-        flags = (audio_output_flags_t)(flags | AUDIO_OUTPUT_FLAG_DIRECT);
+        trackFlags = (audio_output_flags_t)(flags | AUDIO_OUTPUT_FLAG_DIRECT);
     }
     size_t temp = frameCount;   // temp may be replaced by a revised value of frameCount,
                                 // but we will still need the original value also
@@ -1421,7 +1422,7 @@ status_t AudioTrack::createTrack_l()
                                                       mFormat,
                                                       mChannelMask,
                                                       &temp,
-                                                      &flags,
+                                                      &trackFlags,
                                                       mSharedBuffer,
                                                       output,
                                                       mClientPid,
