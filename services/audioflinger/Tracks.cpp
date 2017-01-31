1da5859d05267b336325f2662a72c1456f61e430
Andy Hung <hunga@google.com>
2017-01-30 17:44:03 -0800

    DO NOT MERGE Revert "Log audio information to ensure complete delivery"
    This reverts commit 1f82f9569d257adc1d6d1c9a58e542c3c99c0109.

--- services/audioflinger/Tracks.cpp
+++ services/audioflinger/Tracks.cpp
@@ -461,7 +461,7 @@ void AudioFlinger::PlaybackThread::Track::destroy()
 /*static*/ void AudioFlinger::PlaybackThread::Track::appendDumpHeader(String8& result)
 {
     result.append("    Name Active Client Type      Fmt Chn mask Session fCount S F SRate  "
-                  "L dB  R dB    Server Main buf  Aux buf Flags UndFrmCnt  Flushed\n");
+                  "L dB  R dB    Server Main buf  Aux Buf Flags UndFrmCnt\n");
 }
 
 void AudioFlinger::PlaybackThread::Track::dump(char* buffer, size_t size, bool active)
@@ -528,7 +528,7 @@ void AudioFlinger::PlaybackThread::Track::dump(char* buffer, size_t size, bool a
         break;
     }
     snprintf(&buffer[8], size-8, " %6s %6u %4u %08X %08X %7u %6zu %1c %1d %5u %5.2g %5.2g  "
-                                 "%08X %08zX %08zX 0x%03X %9u%c %7u\n",
+                                 "%08X %p %p 0x%03X %9u%c\n",
             active ? "yes" : "no",
             (mClient == 0) ? getpid_cached : mClient->pid(),
             mStreamType,
@@ -542,12 +542,11 @@ void AudioFlinger::PlaybackThread::Track::dump(char* buffer, size_t size, bool a
             20.0 * log10(float_from_gain(gain_minifloat_unpack_left(vlr))),
             20.0 * log10(float_from_gain(gain_minifloat_unpack_right(vlr))),
             mCblk->mServer,
-            (size_t)mMainBuffer, // use %zX as %p appends 0x
-            (size_t)mAuxBuffer,  // use %zX as %p appends 0x
+            mMainBuffer,
+            mAuxBuffer,
             mCblk->mFlags,
             mAudioTrackServerProxy->getUnderrunFrames(),
-            nowInUnderrun,
-            (unsigned)mAudioTrackServerProxy->framesFlushed() % 10000000); // 7 digits
+            nowInUnderrun);
 }
 
 uint32_t AudioFlinger::PlaybackThread::Track::sampleRate() const {
