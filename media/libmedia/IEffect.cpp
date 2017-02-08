cce0073733f08f8a433d71258772938fd3b830f6
xpduyson <xpduyson@gmail.com>
2017-02-08 18:13:28 +0700

    Fix security vulnerability: potential OOB write in audioserver
    Bug: 32705438
    Bug: 32703959
    Test: cts security test
    Change-Id: I8900c92fa55b56c4c2c9d721efdbabe6bfc8a4a4
    (cherry picked from commit e275907e576601a3579747c3a842790bacf111e2)
    (cherry picked from commit b0bcddb44d992e74140a3f5eedc7177977ea8e34)

--- media/libmedia/IEffect.cpp
+++ media/libmedia/IEffect.cpp
@@ -25,6 +25,9 @@
 
 namespace android {
 
+// Maximum command/reply size expected
+#define EFFECT_PARAM_SIZE_MAX       65536
+
 enum {
     ENABLE = IBinder::FIRST_CALL_TRANSACTION,
     DISABLE,
@@ -153,6 +156,10 @@ status_t BnEffect::onTransact(
             uint32_t cmdSize = data.readInt32();
             char *cmd = NULL;
             if (cmdSize) {
+                if (cmdSize > EFFECT_PARAM_SIZE_MAX) {
+                    reply->writeInt32(NO_MEMORY);
+                    return NO_ERROR;
+                }
                 cmd = (char *)calloc(cmdSize, 1);
                 if (cmd == NULL) {
                     reply->writeInt32(NO_MEMORY);
@@ -164,6 +171,11 @@ status_t BnEffect::onTransact(
             uint32_t replySz = replySize;
             char *resp = NULL;
             if (replySize) {
+                if (replySize > EFFECT_PARAM_SIZE_MAX) {
+                    free(cmd);
+                    reply->writeInt32(NO_MEMORY);
+                    return NO_ERROR;
+                }
                 resp = (char *)calloc(replySize, 1);
                 if (resp == NULL) {
                     free(cmd);
