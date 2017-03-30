2a6e1b0211e3337931c65a33e07c344719ca4328
Mateusz Kaplon <mateusz.kaplon@sony.com>
2017-03-30 16:50:50 +0200

    AudioFlinger: improve purgeStaleEffects_l thread-safety
    There was a situation when a thread in audioserver
    called ThreadBase::disconnectEffectHandle that would
    remove effect chain objects from system in the same
    time that purgeStaleEffects_l would do effect chain
    discovery. This caused race conditions and crash
    in purgeStaleEffects_l.
    
    This patch fixes that by putting code that handles
    discovery of effect chains in playback and record threads
    under mutex of currently checked ThreadBase. This
    ensures that any other thread that currently removes
    effect objects from system finishes before
    purgeStaleEffects_l proceeds with effect discovery.
    
    Bug: 65535528
    Test: Check Audio Effects works correctly.
    Change-Id: Ia7daac6aaf16c4099ef569e3d438197ac7aad5d4

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -2577,6 +2577,7 @@ void AudioFlinger::purgeStaleEffects_l() {
 
     for (size_t i = 0; i < mPlaybackThreads.size(); i++) {
         sp<PlaybackThread> t = mPlaybackThreads.valueAt(i);
+        Mutex::Autolock _l(t->mLock);
         for (size_t j = 0; j < t->mEffectChains.size(); j++) {
             sp<EffectChain> ec = t->mEffectChains[j];
             if (ec->sessionId() > AUDIO_SESSION_OUTPUT_MIX) {
@@ -2586,6 +2587,7 @@ void AudioFlinger::purgeStaleEffects_l() {
     }
     for (size_t i = 0; i < mRecordThreads.size(); i++) {
         sp<RecordThread> t = mRecordThreads.valueAt(i);
+        Mutex::Autolock _l(t->mLock);
         for (size_t j = 0; j < t->mEffectChains.size(); j++) {
             sp<EffectChain> ec = t->mEffectChains[j];
             chains.push(ec);
