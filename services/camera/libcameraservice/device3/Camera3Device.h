67a0c0e77dce4dccc9155f4bfce490d167457c8a
Chia-I Wu <olv@google.com>
2017-04-06 13:37:01 -0700

    libcameraservice: update for revised HIDL gralloc
    There is only BufferUsage instead of ProducerUsage/ConsumerUsage
    now.
    
    Test: make
    Change-Id: I265ca4c27e284fb1f704659efd74355f82d3c192

--- services/camera/libcameraservice/device3/Camera3Device.h
+++ services/camera/libcameraservice/device3/Camera3Device.h
@@ -598,7 +598,7 @@ class Camera3Device :
     static hardware::graphics::common::V1_0::PixelFormat mapToPixelFormat(int frameworkFormat);
     static hardware::camera::device::V3_2::DataspaceFlags mapToHidlDataspace(
             android_dataspace dataSpace);
-    static hardware::camera::device::V3_2::ConsumerUsageFlags mapToConsumerUsage(uint32_t usage);
+    static hardware::camera::device::V3_2::BufferUsageFlags mapToConsumerUsage(uint32_t usage);
     static hardware::camera::device::V3_2::StreamRotation mapToStreamRotation(
             camera3_stream_rotation_t rotation);
     // Returns a negative error code if the passed-in operation mode is not valid.
@@ -607,9 +607,9 @@ class Camera3Device :
     static camera3_buffer_status_t mapHidlBufferStatus(hardware::camera::device::V3_2::BufferStatus status);
     static int mapToFrameworkFormat(hardware::graphics::common::V1_0::PixelFormat pixelFormat);
     static uint32_t mapConsumerToFrameworkUsage(
-            hardware::camera::device::V3_2::ConsumerUsageFlags usage);
+            hardware::camera::device::V3_2::BufferUsageFlags usage);
     static uint32_t mapProducerToFrameworkUsage(
-            hardware::camera::device::V3_2::ProducerUsageFlags usage);
+            hardware::camera::device::V3_2::BufferUsageFlags usage);
 
     struct RequestTrigger {
         // Metadata tag number, e.g. android.control.aePrecaptureTrigger
