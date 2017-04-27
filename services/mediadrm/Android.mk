6527a56de00c47887e28eee03bbdd28b2472ef2e
John W. Bruce <juce@google.com>
2017-04-27 12:06:44 -0700

    Prefix ENABLE_MEDIADRM_64 with TARGET_
    This build-time flag should be prefixed.
    
    Bug: 37039525
    Test: Built OS
    Change-Id: I09ab0a1ed672a88db5741102d87973eb9267efb3

--- services/mediadrm/Android.mk
+++ services/mediadrm/Android.mk
@@ -43,8 +43,9 @@ LOCAL_MODULE:= mediadrmserver
 
 # TODO: Some legacy DRM plugins only support 32-bit. They need to be migrated to
 # 64-bit. (b/18948909) Once all of a device's legacy DRM plugins support 64-bit,
-# that device can turn on ENABLE_MEDIADRM_64 to build this service as 64-bit.
-ifneq ($(ENABLE_MEDIADRM_64), true)
+# that device can turn on TARGET_ENABLE_MEDIADRM_64 to build this service as
+# 64-bit.
+ifneq ($(TARGET_ENABLE_MEDIADRM_64), true)
 LOCAL_32_BIT_ONLY := true
 endif
 
