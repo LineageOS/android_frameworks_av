89237fa20d47686ceae607051f4c68dea6098991
Sauhard Pande <sauhardp@codeaurora.org>
2017-05-11 20:05:50 +0530

    Camera: CameraHardwareInterface changes to support Extended FD
    Added QDataCallback definition to pass the extended QCameraFrameMetadata
    fields from HIDL to framework.
    
    Change-Id: Idece64f22dd6ee3713c7db658b8583854086a1d8

--- services/camera/libcameraservice/device1/CameraHardwareInterface.cpp
+++ services/camera/libcameraservice/device1/CameraHardwareInterface.cpp
@@ -136,6 +136,20 @@ hardware::Return<void> CameraHardwareInterface::dataCallback(
     return hardware::Void();
 }
 
+hardware::Return<void> CameraHardwareInterface::QDataCallback(
+        DataCallbackMsg msgType, uint32_t data, uint32_t bufferIndex,
+        const vendor::qti::hardware::camera::device::V1_0::QCameraFrameMetadata& metadata) {
+    if (mHidlMemPoolMap.count(data) == 0) {
+        ALOGE("%s: memory pool ID %d not found", __FUNCTION__, data);
+        return hardware::Void();
+    }
+    camera_frame_metadata_t md;
+    md.number_of_faces = metadata.faces.size();
+    md.faces = (camera_face_t*) metadata.faces.data();
+    sDataCb((int32_t) msgType, mHidlMemPoolMap.at(data), bufferIndex, &md, this);
+    return hardware::Void();
+}
+
 hardware::Return<void> CameraHardwareInterface::dataCallbackTimestamp(
         DataCallbackMsg msgType, uint32_t data,
         uint32_t bufferIndex, int64_t timestamp) {
