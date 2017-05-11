9fe2d6677153f7a372e959821ac41e5c73f5978f
Cong Jiajia <jcong@codeaurora.org>
2017-05-11 18:26:13 +0800

    libstagefright: encoder must exist when source starting
    when trying to start source, need to make sure encoder still exist.
    Although encoder starts before source, it may encounter error and be
    released before starting source, this will cause camera hardware can't
    be released.
    
    Change-Id: I5d5d474c5479001ec9d1d97a67c268adbf153337

--- media/libstagefright/MediaCodecSource.cpp
+++ media/libstagefright/MediaCodecSource.cpp
@@ -761,8 +761,8 @@ status_t MediaCodecSource::feedEncoderInputBuffers() {
 }
 
 status_t MediaCodecSource::onStart(MetaData *params) {
-    if (mStopping) {
-        ALOGE("Failed to start while we're stopping");
+    if (mStopping || mOutput.lock()->mEncoderReachedEOS) {
+        ALOGE("Failed to start while we're stopping or encoder already stopped due to EOS error");
         return INVALID_OPERATION;
     }
     int64_t startTimeUs;
