a2cef0d2f41de1b2c13791a49c877c1dbcb79486
Gaoxiang Chen <gaochen@codeaurora.org>
2017-06-21 12:57:08 +0530

    Camera: Skip stream size check for whitelisted apps.
    Issue:
    For quadracfa capture, Blob/YUV output streams need to be
    configured with custom dimensions which will not be available
    in advertised stream configurations map.
    
    Fix:
    Skip the stream size check for whitelisted apps to allow
    configuration of streams with custom dimensions.
    
    Setprop to be used:
    adb shell setprop persist.camera.privapp.list <pack1,pack2>
    
    CRs-Fixed: 2082635
    Change-Id: Id94b40c94f42bf4579dc6d8bb6273003312ea669

--- services/camera/libcameraservice/api2/CameraDeviceClient.cpp
+++ services/camera/libcameraservice/api2/CameraDeviceClient.cpp
@@ -76,7 +76,15 @@ CameraDeviceClient::CameraDeviceClient(const sp<CameraService>& cameraService,
                 cameraId, cameraFacing, clientPid, clientUid, servicePid),
     mInputStream(),
     mStreamingRequestId(REQUEST_ID_NONE),
-    mRequestIdCounter(0) {
+    mRequestIdCounter(0),
+    mPrivilegedClient(false) {
+
+    char value[PROPERTY_VALUE_MAX];
+    property_get("persist.camera.privapp.list", value, "");
+    String16 packagelist(value);
+    if (packagelist.contains(clientPackageName.string())) {
+        mPrivilegedClient = true;
+    }
 
     ATRACE_CALL();
     ALOGI("CameraDeviceClient %s: Opened", cameraId.string());
@@ -785,7 +793,7 @@ binder::Status CameraDeviceClient::createSurfaceFromGbp(
     uint64_t allowedFlags = GraphicBuffer::USAGE_SW_READ_MASK |
                            GraphicBuffer::USAGE_HW_TEXTURE |
                            GraphicBuffer::USAGE_HW_COMPOSER;
-    bool flexibleConsumer = (consumerUsage & disallowedFlags) == 0 &&
+    bool flexibleConsumer = !mPrivilegedClient && (consumerUsage & disallowedFlags) == 0 &&
             (consumerUsage & allowedFlags) != 0;
 
     surface = new Surface(gbp, useAsync);
