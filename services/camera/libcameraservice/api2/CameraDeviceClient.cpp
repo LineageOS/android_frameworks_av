762a66289060f6352cbe3d7c5d1e6d58c59ae294
Gaoxiang Chen <gaochen@codeaurora.org>
2017-06-21 12:57:08 +0530

    Camera: Skip stream size check for whitelisted apps.
    Issue:
    For quadracfa capture, Blob/YUV output streams need to be
    configured with custom dimensions which will not be available
    in advertised stream configurations map.
    
    Fix:
    Skip the stream size check for whitelisted apps to allow
    configuration of streams with custom dimensions.
    
    Setprop to be used:
    adb shell setprop persist.vendor.camera.privapp.list <pack1,pack2>
    
    Change-Id: Id94b40c94f42bf4579dc6d8bb6273003312ea669

--- services/camera/libcameraservice/api2/CameraDeviceClient.cpp
+++ services/camera/libcameraservice/api2/CameraDeviceClient.cpp
@@ -100,7 +100,15 @@ CameraDeviceClient::CameraDeviceClient(const sp<CameraService>& cameraService,
     mInputStream(),
     mStreamingRequestId(REQUEST_ID_NONE),
     mRequestIdCounter(0),
+    mPrivilegedClient(false),
     mOverrideForPerfClass(overrideForPerfClass) {
+
+    char value[PROPERTY_VALUE_MAX];
+    property_get("persist.vendor.camera.privapp.list", value, "");
+    String16 packagelist(value);
+    if (packagelist.contains(clientPackageName.string())) {
+        mPrivilegedClient = true;
+    }
 
     ATRACE_CALL();
     ALOGI("CameraDeviceClient %s: Opened", cameraId.string());
