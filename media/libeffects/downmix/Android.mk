3734857f24184c0b31cf135222972c1d48b20398
rago <rago@google.com>
2017-07-25 11:25:49 -0700

    Revert float capability for downmixer
    Revert floating point processing on downmixer.
    
    This commits partially reverts d7d013446a64c6de9f0f2dfe098a721b140e0b48
    Bug: 63760315
    
    Test: Play multichannel movie
    Change-Id: I16edf8767e02ef5d8ff63f52e8d1222ce0d09e22
    (cherry picked from commit b636c4fd77c445887eb785f3d94c2aa080572b13)

--- media/libeffects/downmix/Android.mk
+++ media/libeffects/downmix/Android.mk
@@ -20,7 +20,8 @@ LOCAL_C_INCLUDES := \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils)
 
-LOCAL_CFLAGS += -fvisibility=hidden -DBUILD_FLOAT
+#-DBUILD_FLOAT
+LOCAL_CFLAGS += -fvisibility=hidden
 LOCAL_CFLAGS += -Wall -Werror
 
 LOCAL_HEADER_LIBRARIES += libhardware_headers
