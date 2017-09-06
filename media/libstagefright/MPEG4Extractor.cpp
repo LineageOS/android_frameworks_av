12586d1c216c52efbfd2265d240147b1c31fe543
Marco Nelissen <marcone@google.com>
2017-09-06 15:14:55 -0700

    Fix memory leaks
    Bug: 64424279
    Change-Id: I814f81d8e050132be7779310d3f1ab6b19f2e593

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -4014,6 +4014,8 @@ status_t MPEG4Source::read(
         }
         if (size > mBuffer->size()) {
             ALOGE("buffer too small: %zu > %zu", size, mBuffer->size());
+            mBuffer->release();
+            mBuffer = NULL;
             return ERROR_BUFFER_TOO_SMALL;
         }
     }
@@ -4300,6 +4302,8 @@ status_t MPEG4Source::fragmentedRead(
         }
         if (size > mBuffer->size()) {
             ALOGE("buffer too small: %zu > %zu", size, mBuffer->size());
+            mBuffer->release();
+            mBuffer = NULL;
             return ERROR_BUFFER_TOO_SMALL;
         }
     }
