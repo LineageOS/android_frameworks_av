39fdbd097a147b5c719dac9ad2759e6c44eb3a4e
Kevin Rocard <krocard@google.com>
2017-11-13 11:15:27 -0800

    IAudioPolicyService: Add attribute tags sanitization
    When audio_attributes_t was read from the binder parcel,
    the string tags field was copied without checking that
    it contained a '\0'.
    
    This could lead to read past the end when tags were used.
    
    This patch always adds a '\0' at the end of the buffer when
    deserializing.
    
    Bug: 68953950
    Test: manual playback/record
    Test: send binder payload without \0 in tags attribute, check that only
          AUDIO_ATTRIBUTES_TAGS_MAX_SIZE - 1 char are printed.
    Change-Id: I285258cbf7cfaf26b191d1f31b3b1e2d724c4934
    Merged-In: I285258cbf7cfaf26b191d1f31b3b1e2d724c4934
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- media/libaudioclient/include/media/IAudioPolicyService.h
+++ media/libaudioclient/include/media/IAudioPolicyService.h
@@ -183,6 +183,8 @@ public:
                                     const Parcel& data,
                                     Parcel* reply,
                                     uint32_t flags = 0);
+private:
+    void sanetizeAudioAttributes(audio_attributes_t* attr);
 };
 
 // ----------------------------------------------------------------------------
