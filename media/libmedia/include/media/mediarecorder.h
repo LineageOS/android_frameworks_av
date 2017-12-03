545868799454063eae7ae219dbf10ee42ce35d0d
Ethan Chen <intervigil@gmail.com>
2017-12-03 15:04:43 -0800

    HAX: add LPCM to list
    Change-Id: I7b53d9f7f10e3cc5ab117be3b35fa0af94ec84d7

--- media/libmedia/include/media/mediarecorder.h
+++ media/libmedia/include/media/mediarecorder.h
@@ -88,6 +88,7 @@ enum audio_encoder {
     AUDIO_ENCODER_HE_AAC = 4,
     AUDIO_ENCODER_AAC_ELD = 5,
     AUDIO_ENCODER_VORBIS = 6,
+    AUDIO_ENCODER_LPCM = 12,
 
     AUDIO_ENCODER_LIST_END // must be the last - used to validate the audio encoder type
 };
