c38b3a326b85418b7fc597573099debb321a85ab
Chih-Hung Hsieh <chh@google.com>
2017-12-05 10:02:22 -0800

    Use -Werror in frameworks/av/media/libstagefright/codecs/avcdec
    * Remove unused variables.
    * Fix print format mismatch warning.
    
    Bug: 66996870
    Test: build with WITH_TIDY=1
    Change-Id: I96c794ef3d44fe22a3c3e682eb1647df8f75d23a

--- media/libstagefright/codecs/avcdec/SoftAVCDec.cpp
+++ media/libstagefright/codecs/avcdec/SoftAVCDec.cpp
@@ -304,7 +304,6 @@ status_t SoftAVC::initDecoder() {
 }
 
 status_t SoftAVC::deInitDecoder() {
-    size_t i;
     IV_API_CALL_STATUS_T status;
 
     if (mCodecCtx) {
@@ -451,7 +450,6 @@ void SoftAVC::onPortFlushCompleted(OMX_U32 portIndex) {
             ivd_video_decode_ip_t s_dec_ip;
             ivd_video_decode_op_t s_dec_op;
             IV_API_CALL_STATUS_T status;
-            size_t sizeY, sizeUV;
 
             setDecodeArgs(&s_dec_ip, &s_dec_op, NULL, NULL, 0);
 
@@ -563,7 +561,6 @@ void SoftAVC::onQueueFilled(OMX_U32 portIndex) {
             ivd_video_decode_ip_t s_dec_ip;
             ivd_video_decode_op_t s_dec_op;
             nsecs_t timeDelay, timeTaken;
-            size_t sizeY, sizeUV;
 
             if (!setDecodeArgs(&s_dec_ip, &s_dec_op, inHeader, outHeader, timeStampIx)) {
                 ALOGE("Decoder arg setup failed");
