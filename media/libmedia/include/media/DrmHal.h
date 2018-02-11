d3eece8aa39560e8458ba8231b8684d99f11ee6b
Tobias Thierer <tobiast@google.com>
2018-02-11 15:22:12 +0000

    Revert "Update drm hal to find drm@1.1 factories"
    This reverts commit a3c77911df570d4378ae4869d1060915001f0eba.
    
    Reason for revert: build cop here. The original CL broke build git_master / walleye_asan, hence I'm reverting.
    
    https://android-build.googleplex.com/builds/submitted/4597721/walleye_asan-userdebug/latest/view/logs/build_error.log
    
    In file included from vendor/google/tools/security/fuzzers/libFuzzer/ICrypto_fuzzer/ICrypto_fuzzer.cpp:24:
    frameworks/av/include/media/CryptoHal.h:23:10: fatal error: 'android/hardware/drm/1.1/ICryptoFactory.h' file not found
    
    Change-Id: I185d5310cadb9990b864d71e42bba94f4740160c

--- media/libmedia/include/media/DrmHal.h
+++ media/libmedia/include/media/DrmHal.h
@@ -18,11 +18,10 @@
 
 #define DRM_HAL_H_
 
-#include <android/hardware/drm/1.0/IDrmFactory.h>
 #include <android/hardware/drm/1.0/IDrmPlugin.h>
-#include <android/hardware/drm/1.0/IDrmPluginListener.h>
-#include <android/hardware/drm/1.1/IDrmFactory.h>
 #include <android/hardware/drm/1.1/IDrmPlugin.h>
+#include <android/hardware/drm/1.0/IDrmPluginListener.h>
+#include <android/hardware/drm/1.0/IDrmFactory.h>
 
 #include <media/DrmMetrics.h>
 #include <media/IDrm.h>
