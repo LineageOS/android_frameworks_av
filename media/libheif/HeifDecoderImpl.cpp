6c06af89857ab62b2922bf85ba7cef577fe3173c
Chong Zhang <chz@google.com>
2018-02-27 12:43:06 -0800

    heif: use width and height for decoder -- DO NOT MERGE
    The display dimensions from MPEG4Extractor is applied on top
    of width and height for scaling, it's not for cropping. For
    bitmap uses we have to use width and height, the frame from
    media server is not scaled.
    
    This required porting over some fixes from master to fix the
    display size vs. image size reporting, otherwise images with
    grids may show a black border at the bottom or right.
    
    bug: 73172046
    Test: test app in bug; open folders with heif files in Photos
    and Downloads; CTS MediaMetadataRetrieverTest.
    Change-Id: I3d7f1492d08d48876836ccc05b6eb4de0d0c0f9a
    (cherry picked from commit 49d4aa0a7407ea6632c4a3441c7a4d922d003ea5)

--- media/libheif/HeifDecoderImpl.cpp
+++ media/libheif/HeifDecoderImpl.cpp
@@ -321,8 +321,8 @@ bool HeifDecoderImpl::init(HeifStream* stream, HeifFrameInfo* frameInfo) {
 
     if (frameInfo != nullptr) {
         frameInfo->set(
-                videoFrame->mDisplayWidth,
-                videoFrame->mDisplayHeight,
+                videoFrame->mWidth,
+                videoFrame->mHeight,
                 videoFrame->mRotationAngle,
                 videoFrame->mBytesPerPixel,
                 videoFrame->mIccSize,
@@ -393,8 +393,8 @@ bool HeifDecoderImpl::decode(HeifFrameInfo* frameInfo) {
 
     if (frameInfo != nullptr) {
         frameInfo->set(
-                videoFrame->mDisplayWidth,
-                videoFrame->mDisplayHeight,
+                videoFrame->mWidth,
+                videoFrame->mHeight,
                 videoFrame->mRotationAngle,
                 videoFrame->mBytesPerPixel,
                 videoFrame->mIccSize,
@@ -413,12 +413,12 @@ bool HeifDecoderImpl::getScanline(uint8_t* dst) {
         return false;
     }
     VideoFrame* videoFrame = static_cast<VideoFrame*>(mFrameMemory->pointer());
-    if (mCurScanline >= videoFrame->mDisplayHeight) {
+    if (mCurScanline >= videoFrame->mHeight) {
         ALOGE("no more scanline available");
         return false;
     }
     uint8_t* src = videoFrame->getFlattenedData() + videoFrame->mRowBytes * mCurScanline++;
-    memcpy(dst, src, videoFrame->mBytesPerPixel * videoFrame->mDisplayWidth);
+    memcpy(dst, src, videoFrame->mBytesPerPixel * videoFrame->mWidth);
     return true;
 }
 
@@ -430,8 +430,8 @@ size_t HeifDecoderImpl::skipScanlines(size_t count) {
 
     uint32_t oldScanline = mCurScanline;
     mCurScanline += count;
-    if (mCurScanline > videoFrame->mDisplayHeight) {
-        mCurScanline = videoFrame->mDisplayHeight;
+    if (mCurScanline > videoFrame->mHeight) {
+        mCurScanline = videoFrame->mHeight;
     }
     return (mCurScanline > oldScanline) ? (mCurScanline - oldScanline) : 0;
 }
