71d8129247008b1fe4cf5bd5f4dbd1b3f49e639e
Divya Narayanan Poojary <dnaray@codeaurora.org>
2018-04-02 11:57:32 +0530

    Avoid presentationComplete loop when hardware is paused
    When client calls pause->stop->start and immediately
    calls stop on AudioTrack, prepareTracks_l of
    directOutputThread loops indefinitely with
    presentationComplete call. Reason being framesWritten
    does not get incremented in this case and
    framesWritten never reaches presentationCompleteFrames.
    
    Fix is to avoid presentationComplete loop indefinitely
    when hardware is paused and reset the track and
    flush the hardware.
    
    CRs-Fixed: 2196647
    Change-Id: Ie398a5f39c1784529709426a4a9c036f78687b57

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -5086,12 +5086,14 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::DirectOutputThread::prep
 
                 int64_t framesWritten = mBytesWritten / mFrameSize;
                 if (mStandby || !last ||
-                        track->presentationComplete(framesWritten, audioHALFrames)) {
+                        track->presentationComplete(framesWritten, audioHALFrames) ||
+                        (track->isStopped() && mHwPaused) ) {
                     if (track->isStopping_2()) {
                         track->mState = TrackBase::STOPPED;
                     }
                     if (track->isStopped()) {
                         track->reset();
+                        mFlushPending = true;
                     }
                     tracksToRemove->add(track);
                 }
