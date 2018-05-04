95c192075d5fbeab2445a26bb1237ae07a4f07c2
Paras Nagda <pnagda@codeaurora.org>
2018-05-04 16:13:15 +0530

    stagefright: add changes related to high-framerates in CameraSource
    Below changes are squashed with this change.
    
    Stagefright: Allow setting high-framerates in CameraSource
    ChangeId: If66211dd81b2a08d4df4c6f23e87304e9e7013f4
    
    Stagefright: Allow setting of high-framerates in CameraSource for HSR
    ChangeId: I30cb3b656570de1b615d55c20c0b4f98ae6e0c12
    
    Stagefright: Create CameraSource for HSR
    ChangeId: I7f420f5b15fb3c05bb7f918430ca9b7a630ed18e
    
    Stagefright: Do not skip frames in time-lapse-source for high-speed
    ChangeId: I8420e44ab96484f0d6301c366a24eefc8efeaf0f
    
    media : Changing time stamp manipulation in HFR recording.
    ChangeId: I98cdb14bb2b9c86013df9b2c8f2e558f184b633e
    
    media: Modify timestamps for HFR use case
    ChangeId: I3faf7294f743b1031ccc6624c3348f7e12b339b8
    
    CRs-Fixed: 2226740
    
    Change-Id: I079d880252992b94bd78ac43aed732f5000175d3

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1617,7 +1617,7 @@ status_t StagefrightRecorder::setupCameraSource(
     Size videoSize;
     videoSize.width = mVideoWidth;
     videoSize.height = mVideoHeight;
-    if (mCaptureFpsEnable) {
+    if (mCaptureFpsEnable && mCaptureFps != mFrameRate) {
         if (!(mCaptureFps > 0.)) {
             ALOGE("Invalid mCaptureFps value: %lf", mCaptureFps);
             return BAD_VALUE;
@@ -1765,6 +1765,7 @@ status_t StagefrightRecorder::setupVideoEncoder(
             preferBFrames = false;
             tsLayers = 2; // use at least two layers as resulting video will likely be sped up
         } else if (mCaptureFps > maxPlaybackFps) { // slow-mo
+            format->setInt32("high-frame-rate", 1);
             maxPlaybackFps = mCaptureFps; // assume video will be played back at full capture speed
             preferBFrames = false;
         }
