3f4d4e038e92b3b22ab00fc6d94e3c49605a6c14
Bruno Martins <bgcngm@gmail.com>
2018-05-11 20:55:34 +0100

    [DNM] CameraParameters: Override with custom HTC params
    Change-Id: I7582e393d9beda5b7b679567306aff2a6050d201

--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -173,8 +173,39 @@ const char CameraParameters::FOCUS_MODE_CONTINUOUS_PICTURE[] = "continuous-pictu
 const char CameraParameters::LIGHTFX_LOWLIGHT[] = "low-light";
 const char CameraParameters::LIGHTFX_HDR[] = "high-dynamic-range";
 
+// HTC values
+const char CameraParameters::CONTI_BURST_CAPTURING[] = "contiburst-capturing";
+const char CameraParameters::CONTI_BURST_CAPTURE_DONE[] = "contiburst-done";
+const char CameraParameters::KEY_CONTI_BURST_STATE[] = "contiburst-state";
+const char CameraParameters::KEY_FORCE_USE_AUDIO_ENABLED[] = "forceuseaudio";
+const char CameraParameters::KEY_GPU_EFFECT[] = "GPU-effect";
+const char CameraParameters::KEY_GPU_EFFECT_PARAM_0[] = "GE-param0";
+const char CameraParameters::KEY_GPU_EFFECT_PARAM_1[] = "GE-param1";
+const char CameraParameters::KEY_GPU_EFFECT_PARAM_2[] = "GE-param2";
+const char CameraParameters::KEY_GPU_EFFECT_PARAM_3[] = "GE-param3";
+const char CameraParameters::KEY_SMILEINFO_BYFACE_SUPPORTED[] = "smileinfo-byface-supported";
+
+static String8 get_forced_value(String8 key, String8 value)
+{
+    if (key == "face-detection-values") return String8("off");
+    if (key == "face-detection") return String8("off");
+    return value;
+}
+
+static void add(DefaultKeyedVector<String8,String8> &map, String8 key, String8 value)
+{
+    value = get_forced_value(key, value);
+    map.add(key, value);
+}
+
+static void replaceValueFor(DefaultKeyedVector<String8,String8> &map, String8 key, String8 value)
+{
+    value = get_forced_value(key, value);
+    map.replaceValueFor(key, value);
+}
+
 CameraParameters::CameraParameters()
-                : mMap()
+                : CameraParameters_EXT(this), mMap()
 {
 }
 
@@ -224,12 +255,12 @@ void CameraParameters::unflatten(const String8 &params)
         if (b == 0) {
             // If there's no semicolon, this is the last item.
             String8 v(a);
-            mMap.add(k, v);
+            add(mMap, k, v);
             break;
         }
 
         String8 v(a, (size_t)(b-a));
-        mMap.add(k, v);
+        add(mMap, k, v);
         a = b+1;
     }
 }
@@ -251,7 +282,13 @@ void CameraParameters::set(const char *key, const char *value)
         return;
     }
 
-    mMap.replaceValueFor(String8(key), String8(value));
+    // Qualcomm camera HALs default to delivering an extra zero-exposure frame on HDR.
+    // Android SDK only wants one frame, so disable this unless the app explicitly asks for it.
+    if (!get("hdr-need-1x")) {
+        replaceValueFor(mMap, String8("hdr-need-1x"), String8("false"));
+    }
+
+    replaceValueFor(mMap, String8(key), String8(value));
 }
 
 void CameraParameters::set(const char *key, int value)
@@ -535,6 +572,21 @@ int CameraParameters::previewFormatToEnum(const char* format) {
 
 bool CameraParameters::isEmpty() const {
     return mMap.isEmpty();
+}
+
+int CameraParameters::getBrightnessLumaTargetSet(int *brightness, int *luma) const {
+    const char *p = get("brightness-luma-target-set");
+    *brightness = *luma = -1;
+    if (p == 0) return -1;
+    parse_pair(p, brightness, luma, ',');
+    return 0;
+}
+
+void CameraParameters::getRawSize(int *x, int *y) const {
+    const char *p = get("raw-size");
+    *x = *y = -1;
+    if (p == 0) return;
+    parse_pair(p, x, y, 'x');
 }
 
 }; // namespace android
