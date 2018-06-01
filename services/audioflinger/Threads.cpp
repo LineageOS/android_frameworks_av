a1365253531902c4bfe098dd85dea2534557e136
yidongh <yidongh@codeaurora.org>
2018-06-01 10:31:26 +0800

    audioflinger: do not idle thread if active tracks exist
    For low-latency playback, when track stops, thread is idled
    immediately, causing delay of remaining data written into
    Audio HAL, and pop noise can be heard.
    
    Do not idle thread until data writing finished and no active
    track exists.
    
    CRs-Fixed: 2230663
    Change-Id: Ic7933c289dd69bac3543ec16a4f33d53fe80e8bb

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -3169,7 +3169,7 @@ bool AudioFlinger::PlaybackThread::threadLoop()
             mMixerStatus = prepareTracks_l(&tracksToRemove);
 
             mActiveTracks.updatePowerState(this);
-            if (mMixerStatus == MIXER_IDLE) {
+            if (mMixerStatus == MIXER_IDLE && !mActiveTracks.size()) {
                 onIdleMixer();
             }
 
@@ -4014,7 +4014,6 @@ void AudioFlinger::MixerThread::threadLoop_sleepTime()
             }
         } else {
             mSleepTimeUs = mIdleSleepTimeUs + mIdleTimeOffsetUs;
-            mIdleTimeOffsetUs = 0;
         }
     } else if (mBytesWritten != 0 || (mMixerStatus == MIXER_TRACKS_ENABLED)) {
         // clear out mMixerBuffer or mSinkBuffer, to ensure buffers are cleared
@@ -4028,6 +4027,7 @@ void AudioFlinger::MixerThread::threadLoop_sleepTime()
         ALOGV_IF(mBytesWritten == 0 && (mMixerStatus == MIXER_TRACKS_ENABLED),
                 "anticipated start");
     }
+    mIdleTimeOffsetUs = 0;
     // TODO add standby time extension fct of effect tail
 }
 
