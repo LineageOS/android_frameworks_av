1dd262fa6192173e8a098ad2fbceb0b26f19a827
Paul Keith <javelinanddart@gmail.com>
2018-11-07 17:21:41 +0100

    libstagefright: omx: Add support for loading prebuilt ddp decoder lib
    * This lib additionally supports loading EAC3{,-JOC}, however
      I have never seen anything other than AC3 in the wild
    
    Change-Id: I376211ddb8a518a93fdc64bc4b71d476d1f26479

--- media/libstagefright/omx/SoftOMXPlugin.cpp
+++ media/libstagefright/omx/SoftOMXPlugin.cpp
@@ -61,6 +61,7 @@ static const struct {
     { "OMX.google.flac.decoder", "flacdec", "audio_decoder.flac" },
     { "OMX.google.flac.encoder", "flacenc", "audio_encoder.flac" },
     { "OMX.google.gsm.decoder", "gsmdec", "audio_decoder.gsm" },
+    { "OMX.dolby.ac3.decoder", "ddpdec", "audio_decoder.ac3" },
 };
 
 static const size_t kNumComponents =
