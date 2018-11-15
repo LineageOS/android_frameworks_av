cd0e42759f3820bb42522a7f60396957d40126e4
Dongwon Kang <dwkang@google.com>
2018-11-15 10:49:14 -0800

    MediaPlayer2: replace canOffloadStream with public API.
    This was missed in the previous change, I85c689b029a821
    
    Test: build
    Bug: 112549970
    Change-Id: Id9c30f5a1fc759c635662e5efd0b0d824182f502

--- media/libmediaplayer2/nuplayer2/NuPlayer2.cpp
+++ media/libmediaplayer2/nuplayer2/NuPlayer2.cpp
@@ -1994,7 +1994,7 @@ void NuPlayer2::determineAudioModeChange(const sp<AMessage> &audioFormat) {
     sp<AMessage> videoFormat = mCurrentSourceInfo.mSource->getFormat(false /* audio */);
     audio_stream_type_t streamType = mAudioSink->getAudioStreamType();
     const bool hasVideo = (videoFormat != NULL);
-    bool canOffload = canOffloadStream(
+    bool canOffload = JMediaPlayer2Utils::isOffloadedAudioPlaybackSupported(
             audioMeta, hasVideo, mCurrentSourceInfo.mSource->isStreaming(), streamType)
                     && (mPlaybackSettings.mSpeed == 1.f && mPlaybackSettings.mPitch == 1.f);
 
