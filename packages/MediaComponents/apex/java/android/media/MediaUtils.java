c7c1542ea3fef663c2395404e6f111dc01e625b4
Hyundo Moon <hdmoon@google.com>
2018-11-20 16:49:14 +0900

    Replace hidden KeyEvent.isMediaKey() usages
    This CL copies the hidden KeyEvent.isMediaKey() method into
    MediaController, and replace the usages with it.
    
    Bug: 119789707
    Test: mmm . (under frameworks/av/packages/MediaComponents)
    Change-Id: I60e94c78f603378e5a4e3d54502cca1b36ef4d4e

---
+++ packages/MediaComponents/apex/java/android/media/MediaUtils.java
@@ -0,0 +1,28 @@
+package android.media;
+
+import android.view.KeyEvent;
+
+/**
+ * @hide
+ */
+public class MediaUtils {
+
+    // Keep sync with KeyEvent#isMediaKey().
+    public static boolean isMediaKey(int keyCode) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MEDIA_PLAY:
+            case KeyEvent.KEYCODE_MEDIA_PAUSE:
+            case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+            case KeyEvent.KEYCODE_MUTE:
+            case KeyEvent.KEYCODE_HEADSETHOOK:
+            case KeyEvent.KEYCODE_MEDIA_STOP:
+            case KeyEvent.KEYCODE_MEDIA_NEXT:
+            case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+            case KeyEvent.KEYCODE_MEDIA_REWIND:
+            case KeyEvent.KEYCODE_MEDIA_RECORD:
+            case KeyEvent.KEYCODE_MEDIA_FAST_FORWARD:
+                return true;
+        }
+        return false;
+    }
+}
