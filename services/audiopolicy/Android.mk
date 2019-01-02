littleGnAl/mirror_platform_frameworks_av

Use ICU in libandroidicu
libicuuc and libicui18n are moved into APEX, but
they have no stable ABI due to the version suffix.

Use libandroidicu which provides stable symbol.
See http://go/apex-stable-icu4c-interface for the design.
See http://aosp/801855 for libandroidicu implementation.

Bug: 117094880
Test: m checkbuild
Change-Id: Ida6aac85dfb79bf8e7a3a2540e567ee211279e09

services/audiopolicy/Android.mk
@@ -83,7 +83,7 @@ LOCAL_SHARED_LIBRARIES += libmedia_helper
 LOCAL_SHARED_LIBRARIES += libmediametrics
 
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
-LOCAL_SHARED_LIBRARIES += libicuuc libxml2
+LOCAL_SHARED_LIBRARIES += libandroidicu libxml2
 
 LOCAL_CFLAGS += -DUSE_XML_AUDIO_POLICY_CONF
 endif #ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)

LineageOS/android_frameworks_av

services/audiopolicy/Android.mk
@@ -85,7 +85,7 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_SHARED_LIBRARIES += libmedia_helper
 LOCAL_SHARED_LIBRARIES += libmediametrics
 
-LOCAL_SHARED_LIBRARIES += libhidlbase libicuuc libxml2
+LOCAL_SHARED_LIBRARIES += libhidlbase libxml2
 
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 LOCAL_CFLAGS += -DUSE_XML_AUDIO_POLICY_CONF