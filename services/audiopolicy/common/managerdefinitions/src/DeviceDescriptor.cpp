LineageOS/android_frameworks_av

commit d84e8f7fdf1a61956953660d3af226c3b147e3bb

audio policy: fix legacy Ad2P with advanced codecs
The changes for hybrid A2DP broke advanced codec support for devices
running a HAL V4.0 and no A2DP offload.

Fix: consider that if no encoded format is explicitely listed in the
device descriptor in audio policy configuration file, then no rule on
encoded format should be enforced.

Bug: 123399421
Test: connect Sony MDR-1 headphones and play music.
Change-Id: Idd13cf0e0cdeb71554b23d178e61cc1d82abea60

services/audiopolicy/common/managerdefinitions/src/DeviceDescriptor.cpp
@@ -51,12 +51,6 @@ DeviceDescriptor::DeviceDescriptor(audio_devices_t type, const FormatVector &enc
         mEncodedFormats.add(AUDIO_FORMAT_AC3);
         mEncodedFormats.add(AUDIO_FORMAT_IEC61937);
     }
-    // For backward compatibility always indicate support for SBC and AAC if no
-    // supported format is listed in the configuration file
-    if ((type & AUDIO_DEVICE_OUT_ALL_A2DP) != 0 && mEncodedFormats.isEmpty()) {
-        mEncodedFormats.add(AUDIO_FORMAT_SBC);
-        mEncodedFormats.add(AUDIO_FORMAT_AAC);
-    }
 }
 
 audio_port_handle_t DeviceDescriptor::getId() const
@@ -102,11 +96,19 @@ bool DeviceDescriptor::hasCurrentEncodedFormat() const
     if (!device_has_encoding_capability(type())) {
         return true;
     }
+    if (mEncodedFormats.isEmpty()) {
+        return true;
+    }
+
     return (mCurrentEncodedFormat != AUDIO_FORMAT_DEFAULT);
 }
 
 bool DeviceDescriptor::supportsFormat(audio_format_t format)
 {
+    if (mEncodedFormats.isEmpty()) {
+        return true;
+    }
+
     for (const auto& devFormat : mEncodedFormats) {
         if (devFormat == format) {
             return true;

commit 143339a9c22a17b988d3f3854da66c7a9df15d6a

audio policy: fix regression in USB audio
Fix regression in audio device selection introduced by
commit d84e8f7f

Bug: 123500997
Test: play music over USB and BT A2DP
Change-Id: I25edea0103b5279eb2c49a5c944a72682ad6b288
(cherry picked from commit 04d65587867f39ace15cf4a2c20516208a31ee15)

services/audiopolicy/common/managerdefinitions/src/DeviceDescriptor.cpp
@@ -105,10 +105,6 @@ bool DeviceDescriptor::hasCurrentEncodedFormat() const
 
 bool DeviceDescriptor::supportsFormat(audio_format_t format)
 {
-    if (mEncodedFormats.isEmpty()) {
-        return true;
-    }
-
     for (const auto& devFormat : mEncodedFormats) {
         if (devFormat == format) {
             return true;