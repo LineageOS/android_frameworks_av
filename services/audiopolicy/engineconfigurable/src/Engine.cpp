f5aa58d324bc1d53b7c0ec31edf81814e8553044
Eric Laurent <elaurent@google.com>
2019-02-22 18:20:11 -0800

    Revert "Revert "audiopolicy: moves Stream Volume Curves to Engine""
    This reverts commit efdbe3bfeff7c150d99900fddca005d48048f188.
    
    Test: make

--- services/audiopolicy/engineconfigurable/src/Engine.cpp
+++ services/audiopolicy/engineconfigurable/src/Engine.cpp
@@ -113,7 +113,7 @@ bool Engine::setVolumeProfileForStream(const audio_stream_type_t &stream,
                                        const audio_stream_type_t &profile)
 {
     if (setPropertyForKey<audio_stream_type_t, audio_stream_type_t>(stream, profile)) {
-        getApmObserver()->getVolumeCurves().switchVolumeCurve(profile, stream);
+        switchVolumeCurve(profile, stream);
         return true;
     }
     return false;
