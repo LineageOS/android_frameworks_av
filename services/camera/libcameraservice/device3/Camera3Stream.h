de8ad03bcca4aeab917c1993272c698b472e5056
Shuzhen Wang <shuzhenwang@google.com>
2019-08-22 12:41:12 -0700

    Camera: Make originalDataSpace be const upon stream creation
    The previous logic was wrong(!) where DATASPACE_UNKNOWN shouldn't be
    used as an initializtion condition.
    
    Test: CTS and vendor testing
    Bug: 139820060
    Bug: 151405656
    Change-Id: I6b59d40ff796d48fe1804b45c189004f1ecc8c18
    Merged-In: I6b59d40ff796d48fe1804b45c189004f1ecc8c18
    (cherry picked from 2f5010dfa412c4998ab4047bfd3e20b595def61b)

--- services/camera/libcameraservice/device3/Camera3Stream.h
+++ services/camera/libcameraservice/device3/Camera3Stream.h
@@ -588,7 +588,7 @@ class Camera3Stream :
 
     //Keep track of original dataSpace in case it gets overridden
     bool mDataSpaceOverridden;
-    android_dataspace mOriginalDataSpace;
+    const android_dataspace mOriginalDataSpace;
 
     String8 mPhysicalCameraId;
     nsecs_t mLastTimestamp;
