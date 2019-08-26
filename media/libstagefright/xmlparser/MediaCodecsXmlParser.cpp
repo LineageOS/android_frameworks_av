f0e84162af70ea23acb4e624a7c796c5525bbc00
Brijesh Patel <pbrijesh@codeaurora.org>
2019-08-26 18:32:49 +0530

    xmlparser: Read target version vendor property for vendor
    - mediaserver read system side target version prop
    - mediacodec read vendor side target version prop
    
    CRs-Fixed: 2516144
    
    Change-Id: Ia35225e3c783082a8d9625da990f2436d1b36528

--- media/libstagefright/xmlparser/MediaCodecsXmlParser.cpp
+++ media/libstagefright/xmlparser/MediaCodecsXmlParser.cpp
@@ -133,7 +133,11 @@ std::string getVendorXmlPath(const std::string &path) {
         if (fileExists(vendorPath + std::string(".xml"))) {
             char version[PROP_VALUE_MAX] = {0};
             result = vendorPath + std::string(".xml");
+#ifdef __ANDROID_VNDK__
             property_get("vendor.media.target.version", version, "0");
+#else
+            property_get("vendor.sys.media.target.version", version, "0");
+#endif
             if (atoi(version) > 0) {
                 std::string versionedXml = vendorPath + std::string("_v") +
                                  std::string(version) + std::string(".xml");
