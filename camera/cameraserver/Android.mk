f51a301329f1579d881e8fdfcec406a643b24ac2
Alessandro Astone <ales.astone@gmail.com>
2019-09-11 13:51:49 +0200

    camera: convert cameraserver back to mk
     * so that we can hide it to cameraserver-in-mediaserver devices
    
    Change-Id: Ifab78921b32c38483c0d5f84c17ed597437e4d8b

---
+++ camera/cameraserver/Android.mk
@@ -0,0 +1,46 @@
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	main_cameraserver.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libcameraservice \
+    liblog \
+    libutils \
+    libui \
+    libgui \
+    libbinder \
+    libhidlbase \
+    libhidltransport \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.provider@2.4 \
+    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.device@1.0 \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.device@3.4
+
+LOCAL_MODULE := cameraserver
+LOCAL_32_BIT_ONLY := true
+
+LOCAL_CFLAGS += -Wall -Wextra -Werror -Wno-unused-parameter
+
+LOCAL_INIT_RC := cameraserver.rc
+LOCAL_VINTF_FRAGMENTS := manifest_android.frameworks.cameraservice.service@2.0.xml
+
+include $(BUILD_EXECUTABLE)
