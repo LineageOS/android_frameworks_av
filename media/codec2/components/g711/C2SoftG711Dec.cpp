0bde0d29160389b29939b581c23ab02fa3d6ce78
Ray Essick <essick@google.com>
2019-10-22 13:33:29 -0700

    g711 channel count limits
    correct 'default to 6 channels' to 'default 1; max 6 channels'
    
    Bug: 143163373
    Test: vts-tradefed run vts --module VtsHalMediaC2V1_0Host
    (cherry picked from commit 7a34a8bd9e15ea2b19b08087600bedf6806b8300)
    
    Change-Id: Iadb32a090f018937a719fc581c9f0e7b31adea3a

--- media/codec2/components/g711/C2SoftG711Dec.cpp
+++ media/codec2/components/g711/C2SoftG711Dec.cpp
@@ -73,8 +73,8 @@ public:
 
         addParameter(
                 DefineParam(mChannelCount, C2_PARAMKEY_CHANNEL_COUNT)
-                .withDefault(new C2StreamChannelCountInfo::output(0u, 6))
-                .withFields({C2F(mChannelCount, value).equalTo(1)})
+                .withDefault(new C2StreamChannelCountInfo::output(0u, 1))
+                .withFields({C2F(mChannelCount, value).inRange(1, 6)})
                 .withSetter(Setter<decltype(*mChannelCount)>::StrictValueWithNoDeps)
                 .build());
 
