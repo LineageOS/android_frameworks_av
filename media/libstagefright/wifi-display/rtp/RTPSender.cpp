8b56287e8acfacbc84eb2dd2228d4d6822f4cac8
Nico <nicorg2529@gmail.com>
2019-11-01 15:41:51 +0000

    Partial revert "Move unused classes out of stagefright foundataion"
    This imports the old foundation code in the standard platform stagefright.
    The foundation variant is used in VNDK, we can't change the ABI there.
    
    This reverts commit 5ec3d6ac0c607e89d03ba5a9499e471d8559dc7e.
    
    Change-Id: Iebcf5d89a768fdb830bea90fbf2c2427a4c3d139
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>

--- media/libstagefright/wifi-display/rtp/RTPSender.cpp
+++ media/libstagefright/wifi-display/rtp/RTPSender.cpp
@@ -24,9 +24,9 @@
 #include <media/stagefright/foundation/ABuffer.h>
 #include <media/stagefright/foundation/ADebug.h>
 #include <media/stagefright/foundation/AMessage.h>
-#include <media/stagefright/foundation/ANetworkSession.h>
 #include <media/stagefright/foundation/ByteUtils.h>
 #include <media/stagefright/foundation/hexdump.h>
+#include <media/stagefright/ANetworkSession.h>
 #include <media/stagefright/MediaErrors.h>
 #include <media/stagefright/Utils.h>
 
