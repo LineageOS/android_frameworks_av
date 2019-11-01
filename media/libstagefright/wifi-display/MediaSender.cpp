9ebadfde6663e860d2a5eb6cdc54e07bf7de124a
Nico <nicorg2529@gmail.com>
2019-11-01 15:41:51 +0000

    Partial revert "Move unused classes out of stagefright foundataion"
    This imports the old foundation code in the standard platform stagefright.
    The foundation variant is used in VNDK, we can't change the ABI there.
    
    This reverts commit 5ec3d6ac0c607e89d03ba5a9499e471d8559dc7e.
    
    Change-Id: Iebcf5d89a768fdb830bea90fbf2c2427a4c3d139
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>

--- media/libstagefright/wifi-display/MediaSender.cpp
+++ media/libstagefright/wifi-display/MediaSender.cpp
@@ -26,11 +26,12 @@
 #include "include/avc_utils.h"
 
 #include <media/IHDCP.h>
-#include <media/stagefright/MediaBuffer.h>
 #include <media/stagefright/foundation/ABuffer.h>
 #include <media/stagefright/foundation/ADebug.h>
 #include <media/stagefright/foundation/AMessage.h>
-#include <media/stagefright/foundation/ANetworkSession.h>
+#include <media/stagefright/ANetworkSession.h>
+#include <media/stagefright/MediaBuffer.h>
+#include <media/stagefright/MediaBufferBase.h>
 #include <ui/GraphicBuffer.h>
 
 namespace android {
