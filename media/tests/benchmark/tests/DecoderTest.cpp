littleGnAl/mirror_platform_frameworks_av

Benchmark: Fixing error propgation for Async callback
Test: decoderTest --gtest_repeat=10 -P /sdcard/res/
Test: encoderTest --gtest_repeat=10 -P /sdcard/res/

Bug: 141359753

Change-Id: I8206c92174ce32557bc72b346a41050f7f362fa0

media/tests/benchmark/tests/DecoderTest.cpp
@@ -110,7 +110,7 @@ TEST_P(DecoderTest, Decode) {
         decoder->setupDecoder();
         status = decoder->decode(inputBuffer, frameInfo, codecName, asyncMode);
         if (status != AMEDIA_OK) {
-            cout << "[   WARN   ] Test Skipped. Decode returned error \n";
+            cout << "[   WARN   ] Test Skipped. Decode returned error " << status << endl;
             free(inputBuffer);
             return;
         }

media/tests/benchmark/tests/EncoderTest.cpp
@@ -161,7 +161,11 @@ TEST_P(EncoderTest, Encode) {
         string codecName = get<1>(params);
         bool asyncMode = get<2>(params);
         status = encoder->encode(codecName, eleStream, eleSize, asyncMode, encParams, (char *)mime);
-        ASSERT_EQ(status, 0);
+        if (status != AMEDIA_OK) {
+            cout << "[   WARN   ] Test Skipped. Encode returned error " << status << endl;
+            free(inputBuffer);
+            return;
+        }
         encoder->deInitCodec();
         cout << "codec : " << codecName << endl;
         string inputReference = get<0>(params);

LineageOS/android_frameworks_av

media/tests/benchmark/tests/DecoderTest.cpp
@@ -93,7 +93,7 @@ TEST_P(DecoderTest, Decode) {
         decoder->setupDecoder();
         status = decoder->decode(inputBuffer, frameInfo, codecName, asyncMode);
         if (status != AMEDIA_OK) {
-            cout << "[   WARN   ] Test Skipped. Decode returned error \n";
+            cout << "[   WARN   ] Test Failed. Decode returned error " << status << endl;
             free(inputBuffer);
             return;
         }

media/tests/benchmark/tests/EncoderTest.cpp
@@ -143,7 +143,11 @@ TEST_P(EncoderTest, Encode) {
         string codecName = get<1>(params);
         bool asyncMode = get<2>(params);
         status = encoder->encode(codecName, eleStream, eleSize, asyncMode, encParams, (char *)mime);
-        ASSERT_EQ(status, 0);
+        if (status != AMEDIA_OK) {
+            cout << "[   WARN   ] Test Failed. Encode returned error " << status << endl;
+            free(inputBuffer);
+            return;
+        }
         encoder->deInitCodec();
         cout << "codec : " << codecName << endl;
         string inputReference = get<0>(params);