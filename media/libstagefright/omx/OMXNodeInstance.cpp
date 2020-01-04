f1dcfb3179a2443ed3dfa5463632b4916358cbef
Arne Coucheron <arco68@gmail.com>
2020-01-04 10:32:23 +0100

    Enable legacy adaptive playback for QCOM_BSP_LEGACY
    Adaptive playback got broken in Q, and this is needed
    to fix issues with video playback in apps like YouTube etc.
    
    Change-Id: Ieb440b93222e67043eb5ceda50c0b5eea3198695

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -377,7 +377,11 @@ OMXNodeInstance::OMXNodeInstance(
     mGraphicBufferEnabled[0] = false;
     mGraphicBufferEnabled[1] = false;
     mIsSecure = AString(name).endsWith(".secure");
+#ifdef QCOM_BSP_LEGACY
+    mLegacyAdaptiveExperiment = true;
+#else
     mLegacyAdaptiveExperiment = ADebug::isExperimentEnabled("legacy-adaptive");
+#endif
 }
 
 OMXNodeInstance::~OMXNodeInstance() {
