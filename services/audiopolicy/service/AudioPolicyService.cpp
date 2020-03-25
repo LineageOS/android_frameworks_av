d0e2c74984f7542cc154a899190bda100ef38671
Mikhail Naganov <mnaganov@google.com>
2020-03-25 15:59:59 -0700

    APM: Notify the client about new audio ports
    Added missing notification to the APM client
    about new audio ports. This should make the
    ports provided by the newly discovered HAL
    to be available to apps.
    
    Process the onNewAudioModulesAvailable notification
    on the "APM Output" command thread to avoid clashing
    with outgoing commands from APM during module
    initialization.
    
    Bug: 149854039
    Test: atest audiopolicy_tests
    Change-Id: I3fede7f5a04419b15e15476d8b5ac7dc6d63c98d

--- services/audiopolicy/service/AudioPolicyService.cpp
+++ services/audiopolicy/service/AudioPolicyService.cpp
@@ -1876,7 +1876,7 @@ void AudioPolicyService::setEffectSuspended(int effectId,
 
 void AudioPolicyService::onNewAudioModulesAvailable()
 {
-    mAudioCommandThread->audioModulesUpdateCommand();
+    mOutputCommandThread->audioModulesUpdateCommand();
 }
 
 
