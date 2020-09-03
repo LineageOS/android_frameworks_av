1df51f3fa6cd1a5887cac61d610bbe76af5349c6
Wonsik Kim <wonsik@google.com>
2020-09-03 11:48:41 -0700

    CCodec: don't trigger output format change after flush
    Bug: 149751672
    Test: atest CtsMediaTestCases:AdaptivePlaybackTest
    Merged-In: If34bd5eba1498f081d613d2ebc15ff75f2a90f2f
    Change-Id: If34bd5eba1498f081d613d2ebc15ff75f2a90f2f
    (cherry picked from commit f6c41426c27ed85616a531ff25116333c7794568)

--- media/codec2/sfplugin/CCodec.cpp
+++ media/codec2/sfplugin/CCodec.cpp
@@ -1331,8 +1331,6 @@ void CCodec::start() {
         mCallback->onError(err2, ACTION_CODE_FATAL);
         return;
     }
-    // We're not starting after flush.
-    (void)mSentConfigAfterResume.test_and_set();
     err2 = mChannel->start(inputFormat, outputFormat, buffersBoundToCodec);
     if (err2 != OK) {
         mCallback->onError(err2, ACTION_CODE_FATAL);
@@ -1580,7 +1578,6 @@ void CCodec::signalResume() {
         return;
     }
 
-    mSentConfigAfterResume.clear();
     {
         Mutexed<std::unique_ptr<Config>>::Locked configLocked(mConfig);
         const std::unique_ptr<Config> &config = *configLocked;
@@ -1797,7 +1794,7 @@ void CCodec::onMessageReceived(const sp<AMessage> &msg) {
             // handle configuration changes in work done
             Mutexed<std::unique_ptr<Config>>::Locked configLocked(mConfig);
             const std::unique_ptr<Config> &config = *configLocked;
-            bool changed = !mSentConfigAfterResume.test_and_set();
+            bool changed = false;
             Config::Watcher<C2StreamInitDataInfo::output> initData =
                 config->watch<C2StreamInitDataInfo::output>();
             if (!work->worklets.empty()
