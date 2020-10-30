littleGnAl/mirror_platform_frameworks_av

libstatgefright: nts OPL-001-TC4 test failed [1/1]
Bug:171534171
PD#SWPL-35082

Problem:
For tunnel mode we can't set resolution to drm, so
add it when call queueSecureInputBuffer.

Solutoin:
For tunnel mode we can't set resolution to drm, so
add it when call queueSecureInputBuffer.

Verify:
AndroidQ

Change-Id: Icddb478051402d0b5ac0db29b78222faa96d8aa7
Signed-off-by: shipeng.sun <shipeng.sun@amlogic.com>

media/libstagefright/include/media/stagefright/MediaCodec.h
@@ -505,6 +505,9 @@ private:
     };
 
     Histogram mLatencyHist;
+    int32_t mInputSaveWidth;
+    int32_t mInputSaveHeight;
+    bool mTunneled;
 
     DISALLOW_EVIL_CONSTRUCTORS(MediaCodec);
 };

LineageOS/android_frameworks_av

media/libstagefright/include/media/stagefright/MediaCodec.h
@@ -418,6 +418,10 @@ private:
 
     sp<ICrypto> mCrypto;
 
+    int32_t mTunneledInputWidth;
+    int32_t mTunneledInputHeight;
+    bool mTunneled;
+
     sp<IDescrambler> mDescrambler;
 
     List<sp<ABuffer> > mCSD;