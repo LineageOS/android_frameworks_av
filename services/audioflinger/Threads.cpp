56aa82b2b865d81b9bd7ae532d21224637edef59
wendy lin <wendy.lin@mediatek.com>
2020-12-02 15:19:55 +0800

    Fix exception when Release Audio Input Effect
    Effect is removed while user switch audio output device.
    When updating output device for record thread, exception happens.
    Solution is to get record thread lock before update output devices.
    Holding thread lock will keep effect not being removed while
    updating devices.
    
    Bug: 174714069
    Test: Audio Stress Test
    Change-Id: If831b2322bb02784b0a278abd9ba14b5150ff80b

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -8636,6 +8636,7 @@ status_t AudioFlinger::RecordThread::releaseAudioPatch_l(const audio_patch_handl
 
 void AudioFlinger::RecordThread::updateOutDevices(const DeviceDescriptorBaseVector& outDevices)
 {
+    Mutex::Autolock _l(mLock);
     mOutDevices = outDevices;
     mOutDeviceTypeAddrs = deviceTypeAddrsFromDescriptors(mOutDevices);
     for (size_t i = 0; i < mEffectChains.size(); i++) {
