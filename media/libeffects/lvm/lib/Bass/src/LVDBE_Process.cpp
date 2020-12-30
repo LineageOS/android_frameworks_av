littleGnAl/mirror_platform_frameworks_av

Biquad filter integration to LVM : Continuation
Bug: 120944950
Test: lvmtest
Change-Id: Iab0ebda75a03dadc4edd038a387e087a35b4a83d

media/libeffects/lvm/lib/Bass/src/LVDBE_Process.cpp
@@ -129,7 +129,7 @@ LVDBE_ReturnStatus_en LVDBE_Process(
          */
         if (pInstance->Params.HPFSelect == LVDBE_HPF_ON) {
 #ifdef BIQUAD_OPT
-            pInstance->pBqInstance->process(pScratch, pScratch, NrFrames);
+            pInstance->pBqInstanceHPF->process(pScratch, pScratch, NrFrames);
 #else
             BQ_MC_D32F32C30_TRC_WRA_01(&pInstance->pCoef->HPFInstance, /* Filter instance      */
                                        pScratch,                       /* Source               */
@@ -149,10 +149,14 @@ LVDBE_ReturnStatus_en LVDBE_Process(
         /*
          * Apply the band pass filter
          */
+#ifdef BIQUAD_OPT
+        pInstance->pBqInstanceBPF->process(pMono, pMono, NrFrames);
+#else
         BP_1I_D32F32C30_TRC_WRA_02(&pInstance->pCoef->BPFInstance, /* Filter instance       */
                                    pMono,                          /* Source                */
                                    pMono,                          /* Destination           */
                                    (LVM_INT16)NrFrames);
+#endif
 
         /*
          * Apply the AGC and mix

media/libeffects/lvm/lib/Bundle/src/LVM_Private.h
@@ -33,6 +33,9 @@
 /*                                                                                  */
 /************************************************************************************/
 
+#ifdef BIQUAD_OPT
+#include <audio_utils/BiquadFilter.h>
+#endif
 #include "LVM.h"            /* LifeVibes */
 #include "LVM_Common.h"     /* LifeVibes common */
 #include "BIQUAD.h"         /* Biquad library */
@@ -129,12 +132,16 @@ typedef struct {
 
 /* Filter taps */
 typedef struct {
+#ifndef BIQUAD_OPT
     Biquad_2I_Order1_FLOAT_Taps_t TrebleBoost_Taps; /* Treble boost Taps */
+#endif
 } LVM_TE_Data_t;
 
 /* Coefficients */
 typedef struct {
+#ifndef BIQUAD_OPT
     Biquad_FLOAT_Instance_t TrebleBoost_State; /* State for the treble boost filter */
+#endif
 } LVM_TE_Coefs_t;
 
 typedef struct {
@@ -210,6 +217,10 @@ typedef struct {
     LVM_INT16 NrChannels;
     LVM_INT32 ChMask;
     void* pScratch; /* Pointer to bundle scratch buffer*/
+#ifdef BIQUAD_OPT
+    std::unique_ptr<android::audio_utils::BiquadFilter<LVM_FLOAT>>
+            pBqInstance; /* Biquad filter instance */
+#endif
 
 } LVM_Instance_t;
 

media/libeffects/lvm/lib/Bundle/src/LVM_Process.cpp
@@ -190,9 +190,13 @@ LVM_ReturnStatus_en LVM_Process(LVM_Handle_t hInstance, const LVM_FLOAT* pInData
                 /*
                  * Apply the filter
                  */
+#ifdef BIQUAD_OPT
+                pInstance->pBqInstance->process(pProcessed, pProcessed, NrFrames);
+#else
                 FO_Mc_D16F32C15_LShx_TRC_WRA_01(&pInstance->pTE_State->TrebleBoost_State,
                                                 pProcessed, pProcessed, (LVM_INT16)NrFrames,
                                                 (LVM_INT16)NrChannels);
+#endif
             }
             /*
              * Volume balance

LineageOS/android_frameworks_av

Test: lvmtest with lvm/tests/build_and_run_all_unit_tests.sh

media/libeffects/lvm/lib/Bass/src/LVDBE_Process.cpp
@@ -129,7 +129,7 @@ LVDBE_ReturnStatus_en LVDBE_Process(
          */
         if (pInstance->Params.HPFSelect == LVDBE_HPF_ON) {
 #ifdef BIQUAD_OPT
-            pInstance->pBqInstance->process(pScratch, pScratch, NrFrames);
+            pInstance->pHPFBiquad->process(pScratch, pScratch, NrFrames);
 #else
             BQ_MC_D32F32C30_TRC_WRA_01(&pInstance->pCoef->HPFInstance, /* Filter instance      */
                                        pScratch,                       /* Source               */
@@ -149,10 +149,14 @@ LVDBE_ReturnStatus_en LVDBE_Process(
         /*
          * Apply the band pass filter
          */
+#ifdef BIQUAD_OPT
+        pInstance->pBPFBiquad->process(pMono, pMono, NrFrames);
+#else
         BP_1I_D32F32C30_TRC_WRA_02(&pInstance->pCoef->BPFInstance, /* Filter instance       */
                                    pMono,                          /* Source                */
                                    pMono,                          /* Destination           */
                                    (LVM_INT16)NrFrames);
+#endif
 
         /*
          * Apply the AGC and mix

media/libeffects/lvm/lib/Bundle/src/LVM_Private.h
@@ -33,6 +33,9 @@
 /*                                                                                  */
 /************************************************************************************/
 
+#ifdef BIQUAD_OPT
+#include <audio_utils/BiquadFilter.h>
+#endif
 #include "LVM.h"            /* LifeVibes */
 #include "LVM_Common.h"     /* LifeVibes common */
 #include "BIQUAD.h"         /* Biquad library */
@@ -127,6 +130,7 @@ typedef struct {
     LVM_INT16 SamplesToOutput; /* Samples to write to the output */
 } LVM_Buffer_t;
 
+#ifndef BIQUAD_OPT
 /* Filter taps */
 typedef struct {
     Biquad_2I_Order1_FLOAT_Taps_t TrebleBoost_Taps; /* Treble boost Taps */
@@ -136,6 +140,7 @@ typedef struct {
 typedef struct {
     Biquad_FLOAT_Instance_t TrebleBoost_State; /* State for the treble boost filter */
 } LVM_TE_Coefs_t;
+#endif
 
 typedef struct {
     /* Public parameters */
@@ -185,8 +190,13 @@ typedef struct {
     LVM_INT16 VC_AVLFixedVolume;         /* AVL fixed volume */
 
     /* Treble Enhancement */
+#ifdef BIQUAD_OPT
+    std::unique_ptr<android::audio_utils::BiquadFilter<LVM_FLOAT>>
+            pTEBiquad; /* Biquad filter instance */
+#else
     LVM_TE_Data_t* pTE_Taps;   /* Treble boost Taps */
     LVM_TE_Coefs_t* pTE_State; /* State for the treble boost filter */
+#endif
     LVM_INT16 TE_Active;       /* Control flag */
 
     /* Headroom */

media/libeffects/lvm/lib/Bundle/src/LVM_Process.cpp
@@ -190,9 +190,13 @@ LVM_ReturnStatus_en LVM_Process(LVM_Handle_t hInstance, const LVM_FLOAT* pInData
                 /*
                  * Apply the filter
                  */
+#ifdef BIQUAD_OPT
+                pInstance->pTEBiquad->process(pProcessed, pProcessed, NrFrames);
+#else
                 FO_Mc_D16F32C15_LShx_TRC_WRA_01(&pInstance->pTE_State->TrebleBoost_State,
                                                 pProcessed, pProcessed, (LVM_INT16)NrFrames,
                                                 (LVM_INT16)NrChannels);
+#endif
             }
             /*
              * Volume balance