5f4769e9d5b9067045ad7f1f22df031db9ceba41
Edwin Wong <edwinwong@google.com>
2021-01-05 19:59:11 -0800

    [RESTRICT AUTOMERGE] Fix double free of play policy in a race condition.
    The mPlayPolicy can be freed twice if there is a race condition.
    mPlayPolicy should be protected with a mutex lock.
    
    SafetyNet logging is not added to avoid log spamming. The
    mutex lock is called whenever a license request is made.
    That can happen quite often.
    
    Bug: 176168330
    
    Test: TreeHugger build
    Change-Id: Idccbc768c17676087dd66fcdd568bac402202e8f
    (cherry picked from commit a9249b2326cc16daf70beba36599e4c7e0e7c78b)

--- drm/mediadrm/plugins/clearkey/DrmPlugin.h
+++ drm/mediadrm/plugins/clearkey/DrmPlugin.h
@@ -244,6 +244,7 @@ public:
 private:
     void setPlayPolicy();
 
+    android::Mutex mPlayPolicyLock;
     android::KeyedVector<android::String8, android::String8> mPlayPolicy;
     SessionLibrary* mSessionLibrary;
 
