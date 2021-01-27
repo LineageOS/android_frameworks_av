fc1e79f5bffb54aeff2ac5f2df6e4a96f42aeb5d
Michael W <baddaemon87@gmail.com>
2021-01-27 23:26:01 +0100

    fw/av: Don't try importing ports for invalid devices
    * profile->getSupportedDevices().getDevice() searches for a device
      matching a specific type, address and format
    * if it doesn't find it, we basically get an empty device back, resulting
      in an invalid access in AudioPort::importAudioPort() which then
      iterates over mProfiles of that given devices, resulting in a
      SIGSEGV in audioserver
    * To fix this, check the device we got back and only import it in a valid
      case
    
    Change-Id: Ia6626282f0d62795392e662555b0ecec611e7977

--- services/audiopolicy/common/managerdefinitions/src/HwModule.cpp
+++ services/audiopolicy/common/managerdefinitions/src/HwModule.cpp
@@ -383,10 +383,13 @@ sp<DeviceDescriptor> HwModuleCollection::createDevice(const audio_devices_t type
             const auto &isoTypeDeviceForProfile =
                 profile->getSupportedDevices().getDevice(type, String8(), AUDIO_FORMAT_DEFAULT);
             device->importAudioPort(isoTypeDeviceForProfile, true /* force */);
+            if (isoTypeDeviceForProfile.get() != 0) {
+                device->importAudioPort(isoTypeDeviceForProfile, true /* force */);
 
-            ALOGV("%s: adding device %s to profile %s", __FUNCTION__,
-                  device->toString().c_str(), profile->getTagName().c_str());
-            profile->addSupportedDevice(device);
+                ALOGV("%s: adding device %s to profile %s", __FUNCTION__,
+                      device->toString().c_str(), profile->getTagName().c_str());
+                profile->addSupportedDevice(device);
+            }
         }
     }
     return device;
