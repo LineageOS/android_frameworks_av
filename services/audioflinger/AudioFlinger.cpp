78dc9c8013fd7a3626298d091622dbccb250455d
jiabin <jiabin@google.com>
2021-02-22 19:13:29 +0000

    Add log when starting external vibration.
    Adding log when starting external vibration to help debug
    audio-coupled-haptic playback issues.
    
    Bug: 178431048
    Test: make
    Change-Id: I55c4bf5dad39decb91772f333b111bd93ad1cbdf

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -413,13 +413,18 @@ status_t AudioFlinger::openMmapStream(MmapStreamInterface::stream_direction_t di
 /* static */
 int AudioFlinger::onExternalVibrationStart(const sp<os::ExternalVibration>& externalVibration) {
     sp<os::IExternalVibratorService> evs = getExternalVibratorService();
-    if (evs != 0) {
+    if (evs != nullptr) {
         int32_t ret;
         binder::Status status = evs->onExternalVibrationStart(*externalVibration, &ret);
         if (status.isOk()) {
+            ALOGD("%s, start external vibration with intensity as %d", __func__, ret);
             return ret;
         }
     }
+    ALOGD("%s, start external vibration with intensity as MUTE due to %s",
+            __func__,
+            evs == nullptr ? "external vibration service not found"
+                           : "error when querying intensity");
     return static_cast<int>(os::HapticScale::MUTE);
 }
 
