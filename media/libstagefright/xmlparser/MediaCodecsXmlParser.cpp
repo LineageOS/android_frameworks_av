a727ef9918981a5338c1d328e28c1c4ca7b17ca1
Ray Essick <essick@google.com>
2021-03-29 13:35:02 -0700

    Better tuning hooks for mediaformatshaper
    more appropriate "<Tuning .../>" in Codec XML to indicate parameters.
    enhance per-mediatype default configurations.
    
    Bug: 183211971
    Test: logcat examination
    Test: atest XMLParserTest
    Change-Id: Ieb04ef7b0322fe8a9029b9023dabdd93959e17a2

--- media/libstagefright/xmlparser/MediaCodecsXmlParser.cpp
+++ media/libstagefright/xmlparser/MediaCodecsXmlParser.cpp
@@ -146,7 +146,10 @@ std::vector<std::string> MediaCodecsXmlParser::getDefaultXmlNames() {
         };
     static std::vector<std::string> names = {
             prefixes[0] + variants[0] + ".xml",
-            prefixes[1] + variants[1] + ".xml"
+            prefixes[1] + variants[1] + ".xml",
+
+            // shaping information is not currently variant specific.
+            "media_codecs_shaping.xml"
         };
     return names;
 }
@@ -347,6 +350,7 @@ struct MediaCodecsXmlParser::Impl {
         status_t addFeature(const char **attrs);
         status_t addLimit(const char **attrs);
         status_t addMapping(const char **attrs);
+        status_t addTuning(const char **attrs);
         status_t addQuirk(const char **attrs, const char *prefix = nullptr);
         status_t addSetting(const char **attrs, const char *prefix = nullptr);
         status_t enterMediaCodec(const char **attrs, bool encoder);
@@ -429,7 +433,7 @@ status_t MediaCodecsXmlParser::Impl::parseXmlFilesInSearchDirs(
         if (findFileInDirs(searchDirs, fileName, &path)) {
             err = parseXmlPath(path);
         } else {
-            ALOGD("Cannot find %s in search path", fileName.c_str());
+            ALOGI("Did not find %s in search path", fileName.c_str());
         }
         res = combineStatus(res, err);
     }
@@ -439,7 +443,7 @@ status_t MediaCodecsXmlParser::Impl::parseXmlFilesInSearchDirs(
 status_t MediaCodecsXmlParser::Impl::parseXmlPath(const std::string &path) {
     std::lock_guard<std::mutex> guard(mLock);
     if (!fileExists(path)) {
-        ALOGD("Cannot find %s", path.c_str());
+        ALOGV("Cannot find %s", path.c_str());
         mParsingStatus = combineStatus(mParsingStatus, NAME_NOT_FOUND);
         return NAME_NOT_FOUND;
     }
@@ -743,7 +747,8 @@ void MediaCodecsXmlParser::Impl::Parser::startElementHandler(
             // ignore limits and features specified outside of type
             if (!mState->inType()
                     && (strEq(name, "Limit") || strEq(name, "Feature")
-                        || strEq(name, "Variant") || strEq(name, "Mapping"))) {
+                        || strEq(name, "Variant") || strEq(name, "Mapping")
+                        || strEq(name, "Tuning"))) {
                 PLOGD("ignoring %s specified outside of a Type", name);
                 return;
             } else if (strEq(name, "Limit")) {
@@ -752,6 +757,8 @@ void MediaCodecsXmlParser::Impl::Parser::startElementHandler(
                 err = addFeature(attrs);
             } else if (strEq(name, "Mapping")) {
                 err = addMapping(attrs);
+            } else if (strEq(name, "Tuning")) {
+                err = addTuning(attrs);
             } else if (strEq(name, "Variant") && section != SECTION_VARIANT) {
                 err = limitVariants(attrs);
                 mState->enterSection(err == OK ? SECTION_VARIANT : SECTION_UNKNOWN);
@@ -1442,6 +1449,45 @@ status_t MediaCodecsXmlParser::Impl::Parser::addMapping(const char **attrs) {
     }
 
     mState->addDetail(std::string("mapping-") + a_kind + "-" + a_name, a_value);
+    return OK;
+}
+
+status_t MediaCodecsXmlParser::Impl::Parser::addTuning(const char **attrs) {
+    CHECK(mState->inType());
+    size_t i = 0;
+    const char *a_name = nullptr;
+    const char *a_value = nullptr;
+
+    while (attrs[i] != nullptr) {
+        CHECK((i & 1) == 0);
+        if (attrs[i + 1] == nullptr) {
+            PLOGD("Mapping: attribute '%s' is null", attrs[i]);
+            return BAD_VALUE;
+        }
+
+        if (strEq(attrs[i], "name")) {
+            a_name = attrs[++i];
+        } else if (strEq(attrs[i], "value")) {
+            a_value = attrs[++i];
+        } else {
+            PLOGD("Tuning: ignoring unrecognized attribute '%s'", attrs[i]);
+            ++i;
+        }
+        ++i;
+    }
+
+    // Every tuning must have both fields
+    if (a_name == nullptr) {
+        PLOGD("Tuning with no 'name' attribute");
+        return BAD_VALUE;
+    }
+
+    if (a_value == nullptr) {
+        PLOGD("Tuning with no 'value' attribute");
+        return BAD_VALUE;
+    }
+
+    mState->addDetail(std::string("tuning-") + a_name, a_value);
     return OK;
 }
 
