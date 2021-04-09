ff1b503ba166dc96c88104bf8eb8765ffdf3facf
Andy Hung <hunga@google.com>
2021-04-09 03:03:11 -0700

    Configurable AudioPolicyEngine: Skip device count assert
    As new multi-bit device masks (see also b/167745916) are ignored
    by workaround go/aog/1421703, the number of devices with a
    single bit set cannot exceed 32.
    
    Hence, there is no need for the device count assert in
    buildCommonTypesStructureFile.py. Identical generated
    files are verified by the testOutput.xml generated (all multi-bit
    device masks skipped).
    
    See the log below on exactly which devices are ignored.
    
    Before CL (and adding hdmi_earc):
      INFO:root:Checking Android Header file <_io.TextIOWrapper name='system/media/audio/include/system/audio-hal-enums.h' mode='r' encoding='UTF-8'>
      INFO:root:added stub output device mask
      INFO:root:added stub input device mask
      CRITICAL:root:OutputDevicesMask incompatible with criterion representation on 32 bits
      INFO:root:EXIT ON FAILURE
    
    After CL:
      INFO:root:Checking Android Header file <_io.TextIOWrapper name='system/media/audio/include/system/audio-hal-enums.h' mode='r' encoding='UTF-8'>
      INFO:root:added stub output device mask
      INFO:root:added stub input device mask
      INFO:root:VolumeProfileType: <0:voice_call,1:system,2:ring,3:music,4:alarm,5:notification,6:bluetooth_sco,7:enforced_audible,8:dtmf,9:tts,10:accessibility,11:assistant,12:rerouting,13:patch,14:call_assistant>
      INFO:root:OutputDevicesMask: <1:earpiece,2:speaker,4:wired_headset,8:wired_headphone,16:bluetooth_sco,32:bluetooth_sco_headset,64:bluetooth_sco_carkit,128:bluetooth_a2dp,256:bluetooth_a2dp_headphones,512:bluetooth_a2dp_speaker,1024:hdmi,2048:anlg_dock_headset,4096:dgtl_dock_headset,8192:usb_accessory,16384:usb_device,32768:remote_submix,65536:telephony_tx,131072:line,262144:hdmi_arc,262145:hdmi_earc,524288:spdif,1048576:fm,2097152:aux_line,4194304:speaker_safe,8388608:ip,16777216:bus,33554432:proxy,67108864:usb_headset,134217728:hearing_aid,268435456:echo_canceller,536870912:ble_headset,536870913:ble_speaker,1073741824:stub>
      INFO:root:InputDevicesMask: <1:communication,2:ambient,4:builtin_mic,8:bluetooth_sco_headset,16:wired_headset,32:hdmi,64:telephony_rx,128:back_mic,256:remote_submix,512:anlg_dock_headset,1024:dgtl_dock_headset,2048:usb_accessory,4096:usb_device,8192:fm_tuner,16384:tv_tuner,32768:line,65536:spdif,131072:bluetooth_a2dp,262144:loopback,524288:ip,1048576:bus,16777216:proxy,33554432:usb_headset,67108864:bluetooth_ble,134217728:hdmi_arc,134217729:hdmi_earc,268435456:echo_reference,536870912:ble_headset,1073741824:stub>
      INFO:root:Importing structureTypesFile <_io.TextIOWrapper name='frameworks/av/services/audiopolicy/engineconfigurable/parameter-framework/examples/common/Structure/PolicySubsystem-CommonTypes.xml.in' mode='r' encoding='UTF-8'>
      INFO:root:Device:262145 which has multiple bits set is skipped. b/168065706
      INFO:root:Device:536870913 which has multiple bits set is skipped. b/168065706
      INFO:root:Device:134217729 which has multiple bits set is skipped. b/168065706
    
    Test: frameworks/av/services/audiopolicy/engineconfigurable/tools/buildCommonTypesStructureFile.py \
          --androidaudiobaseheader system/media/audio/include/system/audio-hal-enums.h \
          --commontypesstructure frameworks/av/services/audiopolicy/engineconfigurable/parameter-framework/examples/common/Structure/PolicySubsystem-CommonTypes.xml.in \
          --outputfile testOutput.xml
    Test: Check INFO and compare testOutput.xml is identical
    Bug: 168065706
    Bug: 184939103
    Merged-In: Iaab1baaa89b45e7f3380721629413c987e329132
    Change-Id: Iaab1baaa89b45e7f3380721629413c987e329132
    (cherry picked from commit d8aa090df2f49ead646f7c759cb1f9f592f777ef)

--- services/audiopolicy/engineconfigurable/tools/buildCommonTypesStructureFile.py
+++ services/audiopolicy/engineconfigurable/tools/buildCommonTypesStructureFile.py
@@ -172,12 +172,6 @@ def parseAndroidAudioFile(androidaudiobaseheaderFile):
         logging.info("added stub input device mask")
 
     # Transform input source in inclusive criterion
-    shift = len(all_component_types['OutputDevicesMask'])
-    if shift > 32:
-        logging.critical("OutputDevicesMask incompatible with criterion representation on 32 bits")
-        logging.info("EXIT ON FAILURE")
-        exit(1)
-
     for component_types in all_component_types:
         values = ','.join('{}:{}'.format(value, key) for key, value in all_component_types[component_types].items())
         logging.info("{}: <{}>".format(component_types, values))
