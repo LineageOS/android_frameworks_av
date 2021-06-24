573cb73a1c6c4a8bf13791b98c5d539ebffa2d0f
Pavan Kumar Mc <pmc@codeaurora.org>
2021-06-24 11:19:35 +0530

    Camera: Master callback mode support for MCX raw
    - Raw stream creation failing for size greater than logical
      camera resolution as size cannot exceed max raw stream size
      in available stream configuration.
    
    - Allow creating raw streams of greater size for logical camera
    
    CRs-Fixed: 2968932
    
    Change-Id: Ibacc3c72e34b2f137546288f67bcdb4e0a38aa23

--- services/camera/libcameraservice/utils/SessionConfigurationUtils.h
+++ services/camera/libcameraservice/utils/SessionConfigurationUtils.h
@@ -83,7 +83,7 @@ public:
     // a width <= ROUNDING_WIDTH_CAP
     static bool roundBufferDimensionNearest(int32_t width, int32_t height, int32_t format,
             android_dataspace dataSpace, const CameraMetadata& info, bool maxResolution,
-            /*out*/int32_t* outWidth, /*out*/int32_t* outHeight);
+            /*out*/int32_t* outWidth, /*out*/int32_t* outHeight, bool isPriviledgedClient);
 
     static bool getArrayWidthAndHeight(const CameraMetadata *deviceInfo, int32_t arrayTag,
             int32_t *width, int32_t *height);
