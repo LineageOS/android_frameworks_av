272845c88b8939595dc501a697ca472dd5011a8e
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2022-04-15 09:32:01 -0700

    C2 av1, vp9: Disable P010 temporarily
    Enabling P010 causes a SIGABRT in surfaceflinger. Keep this disabled
    till P010 is fully supported.
    
    Bug: 229387180
    Test: atest CtsMediaDecoderTestCases:\
     android.media.decoder.cts.DecoderTest#testAV1HdrStaticMetadata
    
    Change-Id: Ief436e6b08a1762f4a6e727ee86eb25c225d3cab

--- media/codec2/components/gav1/C2SoftGav1Dec.cpp
+++ media/codec2/components/gav1/C2SoftGav1Dec.cpp
@@ -190,7 +190,8 @@ class C2SoftGav1Dec::IntfImpl : public SimpleInterface<void>::BaseParams {
               .build());
 
     std::vector<uint32_t> pixelFormats = {HAL_PIXEL_FORMAT_YCBCR_420_888};
-    if (isAtLeastT()) {
+    // TODO(b/229387180) Enable once P010 is fully supported
+    if (false && isAtLeastT()) {
         pixelFormats.push_back(HAL_PIXEL_FORMAT_YCBCR_P010);
     }
     // TODO: support more formats?
