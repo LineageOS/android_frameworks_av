d241386b56507acbc0c898573fee4f6443012181
jiabin <jiabin@google.com>
2022-06-15 19:26:01 +0000

    Cache MMAP client silenced state.
    When starting MMAP input stream, APM will check if the client is allowed
    to capture at that moment or not and call setRecordSilenced if the
    client is not allowed. However, the client is not active when starting
    the MMAP input stream. In that case, the client silenced state will be
    lost and the client will be able to capture even though it is not
    allowed. In this CL, when setRecordSilenced is called, it will cache
    the client silenced state so that it can apply when the client is
    active.
    
    Test: atest AAudioTests
    Test: repo steps from the bug
    Bug: 235850634
    Change-Id: I49b5a0f08d1747053f868db6e88c0f677256fc3c
    Merged-In: I49b5a0f08d1747053f868db6e88c0f677256fc3c
    (cherry picked from commit 0960903b2fee5d1d449ffcd598e0b5d3a945d99a)
    (cherry picked from commit 5ee382438971b303a63d07aab40bbdb5a5b88520)
    Merged-In: I49b5a0f08d1747053f868db6e88c0f677256fc3c

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -8646,6 +8646,12 @@ status_t AudioFlinger::MmapThread::start(const AudioClient& client,
     if (isOutput()) {
         ret = AudioSystem::startOutput(portId);
     } else {
+        {
+            // Add the track record before starting input so that the silent status for the
+            // client can be cached.
+            Mutex::Autolock _l(mLock);
+            setClientSilencedStateIfNotExist_l(client.clientUid, false /*silenced*/);
+        }
         ret = AudioSystem::startInput(portId);
     }
 
@@ -8664,6 +8670,7 @@ status_t AudioFlinger::MmapThread::start(const AudioClient& client,
         } else {
             mHalStream->stop();
         }
+        eraseClientSilenceStateIfNoActiveClient_l(client.clientUid);
         return PERMISSION_DENIED;
     }
 
@@ -8671,6 +8678,9 @@ status_t AudioFlinger::MmapThread::start(const AudioClient& client,
     sp<MmapTrack> track = new MmapTrack(this, mAttr, mSampleRate, mFormat, mChannelMask, mSessionId,
                                         isOutput(), client.clientUid, client.clientPid,
                                         IPCThreadState::self()->getCallingPid(), portId);
+    if (!isOutput()) {
+        track->setSilenced_l(isClientSilenced_l(client.clientUid));
+    }
 
     if (isOutput()) {
         // force volume update when a new track is added
@@ -8726,6 +8736,7 @@ status_t AudioFlinger::MmapThread::stop(audio_port_handle_t handle)
     }
 
     mActiveTracks.remove(track);
+    eraseClientSilenceStateIfNoActiveClient_l(track->uid());
 
     mLock.unlock();
     if (isOutput()) {
@@ -9488,6 +9499,7 @@ void AudioFlinger::MmapCaptureThread::setRecordSilenced(uid_t uid, bool silenced
             broadcast_l();
         }
     }
+    setClientSilencedIfExists_l(uid, silenced);
 }
 
 void AudioFlinger::MmapCaptureThread::toAudioPortConfig(struct audio_port_config *config)
