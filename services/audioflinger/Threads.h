cfc10a47f66b2864350aa31a9bf96b76f35b366b
jiabin <jiabin@google.com>
2022-06-15 19:26:01 +0000

    Cache MMAP client silenced state.
    When starting MMAP input stream, APM will check if the client is allowed
    to capture at that moment or not and call setRecordSilenced if the
    client is not allowed. However, the client is not active when starting
    the MMAP input stream. In that case, the client silenced state will be
    lost and the client will be able to capture even though it is not
    allowed. In this CL, when setRecordSilenced is called, it will cache
    the client silenced state so that it can apply when the client is
    active.
    
    Test: atest AAudioTests
    Test: repo steps from the bug
    Bug: 235850634
    Change-Id: I49b5a0f08d1747053f868db6e88c0f677256fc3c
    Merged-In: I49b5a0f08d1747053f868db6e88c0f677256fc3c
    (cherry picked from commit 0960903b2fee5d1d449ffcd598e0b5d3a945d99a)

--- services/audioflinger/Threads.h
+++ services/audioflinger/Threads.h
@@ -1966,6 +1966,26 @@ class MmapThread : public ThreadBase
 
     virtual     bool        isStreamInitialized() { return false; }
 
+                void        setClientSilencedState_l(audio_port_handle_t portId, bool silenced) {
+                                mClientSilencedStates[portId] = silenced;
+                            }
+
+                size_t      eraseClientSilencedState_l(audio_port_handle_t portId) {
+                                return mClientSilencedStates.erase(portId);
+                            }
+
+                bool        isClientSilenced_l(audio_port_handle_t portId) const {
+                                const auto it = mClientSilencedStates.find(portId);
+                                return it != mClientSilencedStates.end() ? it->second : false;
+                            }
+
+                void        setClientSilencedIfExists_l(audio_port_handle_t portId, bool silenced) {
+                                const auto it = mClientSilencedStates.find(portId);
+                                if (it != mClientSilencedStates.end()) {
+                                    it->second = silenced;
+                                }
+                            }
+
  protected:
                 void        dumpInternals_l(int fd, const Vector<String16>& args) override;
                 void        dumpTracks_l(int fd, const Vector<String16>& args) override;
@@ -1985,6 +2005,7 @@ class MmapThread : public ThreadBase
                 AudioHwDevice* const    mAudioHwDev;
                 ActiveTracks<MmapTrack> mActiveTracks;
                 float                   mHalVolFloat;
+                std::map<audio_port_handle_t, bool> mClientSilencedStates;
 
                 int32_t                 mNoCallbackWarningCount;
      static     constexpr int32_t       kMaxNoCallbackWarnings = 5;
