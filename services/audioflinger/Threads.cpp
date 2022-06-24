LineageOS/android_frameworks_av

commit b9cc0ab4c8116ba4fe1c9efb354610edbb0d57ac

AudioFlinger: track metadata fix up
Do not send metadata of tracks used by software patches to audio HAL.
This is true for all record tracks and for playback track with
unspecified usage (stream type).

Bug: 168751366
Test: make
Change-Id: I45a3cc8ecc0e1cca66a613457936306bc21ad90e

services/audioflinger/Threads.cpp
@@ -2961,7 +2961,10 @@ void AudioFlinger::PlaybackThread::updateMetadata_l()
     auto backInserter = std::back_inserter(metadata.tracks);
     for (const sp<Track> &track : mActiveTracks) {
         // No track is invalid as this is called after prepareTrack_l in the same critical section
-        track->copyMetadataTo(backInserter);
+        // Do not forward metadata for PatchTrack with unspecified stream type
+        if (track->streamType() != AUDIO_STREAM_PATCH) {
+            track->copyMetadataTo(backInserter);
+        }
     }
     sendMetadataToBackend_l(metadata);
 }
@@ -8101,6 +8104,10 @@ void AudioFlinger::RecordThread::updateMetadata_l()
     }
     StreamInHalInterface::SinkMetadata metadata;
     for (const sp<RecordTrack> &track : mActiveTracks) {
+        // Do not forward PatchRecord metadata to audio HAL
+        if (track->isPatchTrack()) {
+            continue;
+        }
         // No track is invalid as this is called after prepareTrack_l in the same critical section
         record_track_metadata_v7_t trackMetadata;
         trackMetadata.base = {

commit 49e3928454522f06a96eff632e89d531130d0124

audioflinger: fix metadata for OutputTrack
Fix regression introduced by commit b9cc0ab4 preventing
OutputTracks from forwarding meta data from the duplicating threads to
the downstream mixers.

Bug: 233580950
Test: make
Change-Id: Ide5f5aaf2be73acd32268c2e5483b06a27e8bb75

services/audioflinger/Threads.cpp
@@ -3136,10 +3136,7 @@ void AudioFlinger::PlaybackThread::updateMetadata_l()
     auto backInserter = std::back_inserter(metadata.tracks);
     for (const sp<Track> &track : mActiveTracks) {
         // No track is invalid as this is called after prepareTrack_l in the same critical section
-        // Do not forward metadata for PatchTrack with unspecified stream type
-        if (track->streamType() != AUDIO_STREAM_PATCH) {
-            track->copyMetadataTo(backInserter);
-        }
+        track->copyMetadataTo(backInserter);
     }
     sendMetadataToBackend_l(metadata);
 }

services/audioflinger/Tracks.cpp
@@ -1400,6 +1400,11 @@ void AudioFlinger::PlaybackThread::Track::setFinalVolume(float volume)
 
 void AudioFlinger::PlaybackThread::Track::copyMetadataTo(MetadataInserter& backInserter) const
 {
+    // Do not forward metadata for PatchTrack with unspecified stream type
+    if (mStreamType == AUDIO_STREAM_PATCH) {
+        return;
+    }
+
     playback_track_metadata_v7_t metadata;
     metadata.base = {
             .usage = mAttr.usage,