6dbbb0bff1d0f29e65ed3e547279770f5cc59b1b
Ravneet Dhanjal <rdhanjal@google.com>
2022-06-29 01:34:01 +0000

    CameraService: Updated watchdog disconnect timer
    - Update disconnect watchdog timer to prevent
    thwarting of existing error handling for
    timeouts and prevent from firing earlier than
    intended delays.
    
    
    Bug: 251252726
    Bug: 247397845
    Test: Manual
    Change-Id: Id494157d02dae089a91305f128d9e4f5b660e30f
    (cherry picked from commit 7d412d2cd37510100c1a3e22f2790e96b435ee26)
    (cherry picked from commit 72c966598d239df2445b9d863d7a7884bddd581c)
    Merged-In: Id494157d02dae089a91305f128d9e4f5b660e30f

--- services/camera/libcameraservice/common/Camera2ClientBase.cpp
+++ services/camera/libcameraservice/common/Camera2ClientBase.cpp
@@ -41,8 +41,6 @@
 
 namespace android {
 
-const static size_t kDisconnectTimeoutMs = 2500;
-
 using namespace camera2;
 
 // Interface used by CameraService
@@ -252,10 +250,16 @@ status_t Camera2ClientBase<TClientBase>::dumpDevice(
 
 template <typename TClientBase>
 binder::Status Camera2ClientBase<TClientBase>::disconnect() {
-    if (mCameraServiceWatchdog != nullptr) {
+    if (mCameraServiceWatchdog != nullptr && mDevice != nullptr) {
+        // Timer for the disconnect call should be greater than getExpectedInFlightDuration
+        // since this duration is used to error handle methods in the disconnect sequence
+        // thus allowing existing error handling methods to execute first
+        uint64_t maxExpectedDuration =
+                ns2ms(mDevice->getExpectedInFlightDuration() + kBufferTimeDisconnectNs);
+
         // Initialization from hal succeeded, time disconnect.
         return mCameraServiceWatchdog->WATCH_CUSTOM_TIMER(disconnectImpl(),
-                kDisconnectTimeoutMs / kCycleLengthMs, kCycleLengthMs);
+                maxExpectedDuration / kCycleLengthMs, kCycleLengthMs);
     }
     return disconnectImpl();
 }
