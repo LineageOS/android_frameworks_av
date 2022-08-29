cd8811c1063c7d87670188555d83cf8d774113cf
Shuzhen Wang <shuzhenwang@google.com>
2022-08-29 17:23:52 +0000

    Camera: Make sure metering regions are valid in capture result
    - Make sure ZoomRatioMapper doesn't produce metering rectangles
      with negative width/height
    - If HAL or any framework component produces metering rectangles
      with negative width/height, overwrite it to have 0 width/height.
    
    Test: manual testing, cameraservice_test
    Bug: 243985907
    Change-Id: I04ca487c64f7db3e7ece209def2d1d03bc4a4238

--- services/camera/libcameraservice/tests/ZoomRatioTest.cpp
+++ services/camera/libcameraservice/tests/ZoomRatioTest.cpp
@@ -160,11 +160,9 @@ void subScaleCoordinatesTest(bool usePreCorrectArray) {
             false/*hasZoomRatioRange*/, zoomRatioRange,
             usePreCorrectArray));
 
-    size_t index = 0;
     int32_t width = testActiveArraySize[2];
     int32_t height = testActiveArraySize[3];
     if (usePreCorrectArray) {
-        index = 1;
         width = testPreCorrActiveArraySize[2];
         height = testPreCorrActiveArraySize[3];
     }
@@ -253,6 +251,19 @@ void subScaleCoordinatesTest(bool usePreCorrectArray) {
     mapper.scaleCoordinates(coords.data(), coords.size()/2, 1.0f/3, false /*clamp*/, width, height);
     for (size_t i = 0; i < coords.size(); i++) {
         EXPECT_LE(std::abs(coords[i] - expectedZoomOutCoords[i]), kMaxAllowedPixelError);
+    }
+
+    // Verify region zoom scaling doesn't generate invalid metering region
+    // (width < 0, or height < 0)
+    std::array<float, 3> scaleRatios = {10.0f, 1.0f, 0.1f};
+    for (float scaleRatio : scaleRatios) {
+        for (size_t i = 0; i < originalCoords.size(); i+= 2) {
+            int32_t coordinates[] = {originalCoords[i], originalCoords[i+1],
+                    originalCoords[i], originalCoords[i+1]};
+            mapper.scaleRegion(coordinates, scaleRatio, width, height);
+            EXPECT_LE(coordinates[0], coordinates[2]);
+            EXPECT_LE(coordinates[1], coordinates[3]);
+        }
     }
 }
 
