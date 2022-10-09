1df45ce0340776d846294200a6c861b75e4c8605
cjh1249131356 <cjh1249131356@gmail.com>
2022-10-09 14:45:25 +0800

    StagefrightRecorder: Add "set-title" parameter
    * Required by OnePlus / Oplus Camera.
    * Unused by others.
    
    Change-Id: I173c30d0a948601d6e691e116a205ec545d26c05
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1114,6 +1114,8 @@ status_t StagefrightRecorder::setParameter(
         }
     } else if (key == "log-session-id") {
         return setLogSessionId(value);
+    } else if (key == "set-title") {
+        return OK;
     } else {
         ALOGE("setParameter: failed to find key %s", key.c_str());
     }
