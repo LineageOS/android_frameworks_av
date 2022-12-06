bad75e935993b51606a5bcbaca051cb64daafc1b
Jiabin Huang <jiabin@google.com>
2022-12-06 03:18:10 +0000

    Update tee patches in the thread loop.
    When secondary output is updated, the tee patches need to
    be updated. However, the mixer can access the tee patches
    without any lock. In that case, updating tee patches in the
    threadloop, which is before mixing, can help avoid race
    condition.
    
    Bug: 276314585
    Test: manually
    Test: atest AudioPlaybackCaptureTest
    Change-Id: I36b5ae3777526942a31c7a3122a05477fce0b6df

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -4093,6 +4093,10 @@ bool AudioFlinger::PlaybackThread::threadLoop()
             activeTracks.insert(activeTracks.end(), mActiveTracks.begin(), mActiveTracks.end());
 
             setHalLatencyMode_l();
+
+            for (const auto &track : mActiveTracks ) {
+                track->updateTeePatches();
+            }
         } // mLock scope ends
 
         if (mBytesRemaining == 0) {
