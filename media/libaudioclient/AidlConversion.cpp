6bf08ea6f8a42bf662401a2029295e59920c5ff2
jiabin <jiabin@google.com>
2022-12-13 23:13:13 +0000

    Add encapsulation type pcm.
    Bug: 239435816
    Test: make
    Change-Id: I5970ca52ebfb581adee786ac0c9cbd0e083c6e1c

--- media/libaudioclient/AidlConversion.cpp
+++ media/libaudioclient/AidlConversion.cpp
@@ -3319,6 +3319,8 @@ aidl2legacy_AudioEncapsulationType_audio_encapsulation_type_t(
             return AUDIO_ENCAPSULATION_TYPE_NONE;
         case AudioEncapsulationType::IEC61937:
             return AUDIO_ENCAPSULATION_TYPE_IEC61937;
+        case AudioEncapsulationType::PCM:
+            return AUDIO_ENCAPSULATION_TYPE_PCM;
     }
     return unexpected(BAD_VALUE);
 }
@@ -3331,6 +3333,8 @@ legacy2aidl_audio_encapsulation_type_t_AudioEncapsulationType(
             return AudioEncapsulationType::NONE;
         case AUDIO_ENCAPSULATION_TYPE_IEC61937:
             return AudioEncapsulationType::IEC61937;
+        case AUDIO_ENCAPSULATION_TYPE_PCM:
+            return AudioEncapsulationType::PCM;
     }
     return unexpected(BAD_VALUE);
 }
