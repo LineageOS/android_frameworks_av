630b1366c3172c77c06b1cd0ae5f3345462ed9c8
François Gaffie <francois.gaffie@renault.com>
2023-01-18 18:23:06 +0100

    fixup '[BUG] audio: misalignement of native/java AudioAttributes'
    Bug: 237409207
    Test: atest AudioManagerTest#testAdjustVolumeGroupVolume
    
    Change-Id: I92b5919999ba25d39ec4e16d7601d9f4dcb2b7fe
    Signed-off-by: François Gaffie <francois.gaffie@renault.com>

--- services/audiopolicy/engine/common/include/VolumeGroup.h
+++ services/audiopolicy/engine/common/include/VolumeGroup.h
@@ -39,7 +39,7 @@ public:
     VolumeCurves *getVolumeCurves() { return &mGroupVolumeCurves; }
 
     void addSupportedAttributes(const audio_attributes_t &attr);
-    AttributesVector getSupportedAttributes() const { return mGroupVolumeCurves.getAttributes(); }
+    AttributesVector getSupportedAttributes() const;
 
     void addSupportedStream(audio_stream_type_t stream);
     StreamTypeVector getStreamTypes() const { return mGroupVolumeCurves.getStreamTypes(); }
