dc30665ffb07fb6351594880659c5430ed27671e
Sungtak Lee <taklee@google.com>
2023-02-02 00:59:01 +0000

    GraphicBufferSource: Use 64bit consumer usage
    Bug: 185896428
    Change-Id: I7319e37766f636b86b831f9a2e784f9dde684935

--- media/codec2/hal/client/client.cpp
+++ media/codec2/hal/client/client.cpp
@@ -1683,8 +1683,8 @@ c2_status_t Codec2Client::Component::setOutputSurface(
     uint64_t consumerUsage = kDefaultConsumerUsage;
     {
         if (surface) {
-            int usage = 0;
-            status_t err = surface->query(NATIVE_WINDOW_CONSUMER_USAGE_BITS, &usage);
+            uint64_t usage = 0;
+            status_t err = surface->getConsumerUsage(&usage);
             if (err != NO_ERROR) {
                 ALOGD("setOutputSurface -- failed to get consumer usage bits (%d/%s). ignoring",
                         err, asString(err));
@@ -1697,8 +1697,7 @@ c2_status_t Codec2Client::Component::setOutputSurface(
                 // they do not exist inside of C2 scope. Any buffer usage shall be communicated
                 // through the sideband channel.
 
-                // do an unsigned conversion as bit-31 may be 1
-                consumerUsage = (uint32_t)usage | kDefaultConsumerUsage;
+                consumerUsage = usage | kDefaultConsumerUsage;
             }
         }
 
