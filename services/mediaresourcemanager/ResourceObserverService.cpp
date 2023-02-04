429ef82699ef43f3c5fde0f3375561ec59bbbe2a
Charlie Wang <charliewang@google.com>
2023-02-03 16:15:41 -0800

    Allow media services to be used in isolation.
    Allow media services to be used in isolated processes.
    
    Bug: 266943251
    Test: Services can be retrieved in isolated processes.
    
    Change-Id: Iefe0dcbccc4821742830924d8efd08718271465b

--- services/mediaresourcemanager/ResourceObserverService.cpp
+++ services/mediaresourcemanager/ResourceObserverService.cpp
@@ -100,8 +100,10 @@ void ResourceObserverService::BinderDiedCallback(void* cookie) {
 std::shared_ptr<ResourceObserverService> ResourceObserverService::instantiate() {
     std::shared_ptr<ResourceObserverService> observerService =
             ::ndk::SharedRefBase::make<ResourceObserverService>();
-    binder_status_t status = AServiceManager_addService(observerService->asBinder().get(),
-            ResourceObserverService::getServiceName());
+    binder_status_t status = AServiceManager_addServiceWithAllowIsolated(
+      observerService->asBinder().get(),ResourceObserverService::getServiceName(),
+      /*allowIsolated=*/ true);
+
     if (status != STATUS_OK) {
         return nullptr;
     }
