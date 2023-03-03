littleGnAl/mirror_platform_frameworks_av

MPEG2-TS: Add DTS, DTS-LBR, and DTS:X Profile2 support
* Add support for extracting DTS, DTS-HD and DTS-UHD descriptor ID
  from PSI PMT
* Dequeue and parse DTS(Digital Surround), DTS-LBR(DTS Express),
  and DTS:X Profile-2(UHD Profile 2) frame.

Test: Manaul test
Change-Id: I4496d800b7a4973c64bc3bb6540ed3484304ce2e

media/module/mpeg2ts/include/mpeg2ts/ESQueue.h
@@ -45,6 +45,8 @@ struct ElementaryStreamQueue {
         MPEG4_VIDEO,
         PCM_AUDIO,
         METADATA,
+        DTS_HD,
+        DTS_UHD,
     };
 
     enum Flags {
@@ -125,6 +127,8 @@ private:
     sp<ABuffer> dequeueAccessUnitMPEG4Video();
     sp<ABuffer> dequeueAccessUnitPCMAudio();
     sp<ABuffer> dequeueAccessUnitMetadata();
+    sp<ABuffer> dequeueAccessUnitDTSHD();
+    sp<ABuffer> dequeueAccessUnitDTSUHD();
 
     // consume a logical (compressed) access unit of size "size",
     // returns its timestamp in us (or -1 if no time information).

LineageOS/android_frameworks_av

Bug: 260223911
Test: atest CtsMediaExtractorTestCases

media/module/mpeg2ts/include/mpeg2ts/ESQueue.h
@@ -45,6 +45,9 @@ struct ElementaryStreamQueue {
         MPEG4_VIDEO,
         PCM_AUDIO,
         METADATA,
+        DTS,
+        DTS_HD,
+        DTS_UHD,
     };
 
     enum Flags {
@@ -125,6 +128,8 @@ private:
     sp<ABuffer> dequeueAccessUnitMPEG4Video();
     sp<ABuffer> dequeueAccessUnitPCMAudio();
     sp<ABuffer> dequeueAccessUnitMetadata();
+    sp<ABuffer> dequeueAccessUnitDTSOrDTSHD();
+    sp<ABuffer> dequeueAccessUnitDTSUHD();
 
     // consume a logical (compressed) access unit of size "size",
     // returns its timestamp in us (or -1 if no time information).