dbd069430c8dfcdcd91eabfc96101f4b9c7eef3c
Shunkai Yao <yaoshunkai@google.com>
2023-06-29 18:07:09 +0000

    Support passing entire effect_param_t as vendor extension
    Copy entire effect_param_t into AIDL ParcelableHolder with DefaultExtension
    
    Bug: 271500140
    Test: Enable AIDL
    Test: atest libAudioHalEffectParamTest
    Change-Id: I21829324e820dc68859000d912376a5e1b7d056b

--- media/libaudiohal/impl/effectsAidlConversion/AidlConversionNoiseSuppression.cpp
+++ media/libaudiohal/impl/effectsAidlConversion/AidlConversionNoiseSuppression.cpp
@@ -63,7 +63,7 @@ status_t AidlConversionNoiseSuppression::setParameter(EffectParamReader& param) 
         default: {
             // for vendor extension, copy data area to the DefaultExtension, parameter ignored
             VendorExtension ext = VALUE_OR_RETURN_STATUS(
-                    aidl::android::legacy2aidl_EffectParameterReader_Data_VendorExtension(param));
+                    aidl::android::legacy2aidl_EffectParameterReader_VendorExtension(param));
             aidlParam = MAKE_SPECIFIC_PARAMETER(NoiseSuppression, noiseSuppression, vendor, ext);
             break;
         }
