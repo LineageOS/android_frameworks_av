fcf5248b6c4199b3ea8659d1da455127b70cac93
Tomasz Wasilczyk <twasilczyk@google.com>
2023-08-11 15:52:22 +0000

    Use String8/16 c_str [mtp]
    Bug: 295394788
    Test: make checkbuild
    Change-Id: If578a11f6b6805e1ae9364437d09b3d854fef0df

--- media/mtp/tests/MtpFuzzer/mtp_data_packet_fuzzer.cpp
+++ media/mtp/tests/MtpFuzzer/mtp_data_packet_fuzzer.cpp
@@ -212,7 +212,7 @@ void MtpDataPacketFuzzer::process() {
                     if (mFdp.ConsumeBool()) {
                         std::string str = mFdp.ConsumeRandomLengthString(kMaxLength);
                         android::String16 s(str.c_str());
-                        char16_t* data = const_cast<char16_t*>(s.string());
+                        char16_t* data = const_cast<char16_t*>(s.c_str());
                         mtpDataPacket.putString(reinterpret_cast<uint16_t*>(data));
                     } else {
                         mtpDataPacket.putString(static_cast<uint16_t*>(nullptr));
