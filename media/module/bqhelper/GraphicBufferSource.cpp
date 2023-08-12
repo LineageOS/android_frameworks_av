635cfc6586b216bb1398ff4f966a57c3f6176575
Sungtak Lee <taklee@google.com>
2023-08-12 07:54:01 +0000

    GraphicBufferSource: add configure() with 32bit consumerUsage flag
    Old vendor partition may have references to old
    GraphicBufferSource::configure() and/or
    OmxGraphicBufferSource::configure() with 32bit consumerUsage flag.
    Retain configure() with 32bit consumerUsage flag for GraphicBufferSource
    and OmxGraphicBufferSource from vndk.
    
    Bug: 295498401
    Bug: 185896428
    Test: forrest run https://android-build.googleplex.com/builds/abtd/run/L79000000962568362
    Change-Id: I616a3345cc696f3e8e9094cdbf3848437172f9d4

--- media/module/bqhelper/GraphicBufferSource.cpp
+++ media/module/bqhelper/GraphicBufferSource.cpp
@@ -1150,6 +1150,18 @@ status_t GraphicBufferSource::configure(
         int32_t bufferCount,
         uint32_t frameWidth,
         uint32_t frameHeight,
+        uint32_t consumerUsage) {
+    uint64_t consumerUsage64 = static_cast<uint64_t>(consumerUsage);
+    return configure(component, dataSpace, bufferCount,
+                     frameWidth, frameHeight, consumerUsage64);
+}
+
+status_t GraphicBufferSource::configure(
+        const sp<ComponentWrapper>& component,
+        int32_t dataSpace,
+        int32_t bufferCount,
+        uint32_t frameWidth,
+        uint32_t frameHeight,
         uint64_t consumerUsage) {
     if (component == NULL) {
         return BAD_VALUE;
