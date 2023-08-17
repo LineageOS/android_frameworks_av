5c2d419e6217a96be0db4010c6e4c82ccce10f69
electimon <electimon@gmail.com>
2023-08-17 21:37:47 +0800

    libcameraservice: Allow Moto Camera to skip invalid frames
    test: Take various photos in all modes
    
    Change-Id: Ib9231fe523caead1edbc699f06fda733a94cfb20
    Co-Authroed by: Marc Bourgoin <themard85@gmail.com>
    Signed-off-by: betaxab <betaxab@gmail.com>

--- services/camera/libcameraservice/device3/Camera3OutputUtils.cpp
+++ services/camera/libcameraservice/device3/Camera3OutputUtils.cpp
@@ -1100,7 +1100,8 @@ void notifyShutter(CaptureOutputStates& states, const camera_shutter_msg_t &msg)
                                 states.nextShutterFrameNum, msg.frame_number);
                         return;
                     }
-                    states.nextShutterFrameNum = msg.frame_number + 1;
+                    if (gClientPackageName != "CAM_SKIP_INVALID_FRAMES_PKG" && !(msg.frame_number - states.nextShutterFrameNum > 1))
+                        states.nextShutterFrameNum = msg.frame_number + 1;
                 }
             }
 
