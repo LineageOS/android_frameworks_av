849fb75e85f81f21f769a083956cdf287f635239
Sungtak Lee <taklee@google.com>
2023-11-14 20:46:33 +0000

    Codec2Cient: Do not hold lock during IGBP operations
    In order to minimize lock durations, do not hold locks around
    IGBP operations
    
    Bug: 304183768
    Test: atest android.media.decoder.cts.AdaptivePlaybackTest
    Change-Id: I52729ad8c2b8c54743081fc12c52ff1134d077c1

--- media/codec2/hal/client/output.cpp
+++ media/codec2/hal/client/output.cpp
@@ -141,11 +141,14 @@ status_t attachToBufferQueue(const C2ConstGraphicBlock& block,
                             "status = " << INVALID_OPERATION << ".";
             return INVALID_OPERATION;
         }
-        result = igbp->attachBuffer(bqSlot, graphicBuffer);
-        if (result == OK) {
-            syncVar->notifyDequeuedLocked();
-        }
+        syncVar->notifyDequeuedLocked();
         syncVar->unlock();
+        result = igbp->attachBuffer(bqSlot, graphicBuffer);
+        if (result != OK) {
+            syncVar->lock();
+            syncVar->notifyQueuedLocked();
+            syncVar->unlock();
+        }
     } else {
         result = igbp->attachBuffer(bqSlot, graphicBuffer);
     }
@@ -435,13 +438,13 @@ status_t OutputBufferQueue::outputBuffer(
 
         auto syncVar = syncMem ? syncMem->mem() : nullptr;
         if(syncVar) {
-            syncVar->lock();
             status = outputIgbp->queueBuffer(static_cast<int>(bqSlot),
                                          input, output);
             if (status == OK) {
+                syncVar->lock();
                 syncVar->notifyQueuedLocked();
+                syncVar->unlock();
             }
-            syncVar->unlock();
         } else {
             status = outputIgbp->queueBuffer(static_cast<int>(bqSlot),
                                          input, output);
@@ -490,13 +493,13 @@ status_t OutputBufferQueue::outputBuffer(
     auto syncVar = syncMem ? syncMem->mem() : nullptr;
     status_t status = OK;
     if (syncVar) {
-        syncVar->lock();
         status = outputIgbp->queueBuffer(static_cast<int>(bqSlot),
                                                   input, output);
         if (status == OK) {
+            syncVar->lock();
             syncVar->notifyQueuedLocked();
+            syncVar->unlock();
         }
-        syncVar->unlock();
     } else {
         status = outputIgbp->queueBuffer(static_cast<int>(bqSlot),
                                                   input, output);
