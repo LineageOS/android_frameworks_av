fb650b0de61cb499e906852db9c8af27d880d48b
jiabin <jiabin@google.com>
2023-12-05 00:34:25 +0000

    Add thread safety annotation in libaaudio.
    Bug: 314835808
    Test: make
    Change-Id: I5536b8031b0a5573d80b7fb7a4e85713047a2f3a

--- media/libaaudio/src/client/AudioEndpoint.h
+++ media/libaaudio/src/client/AudioEndpoint.h
@@ -117,8 +117,6 @@ private:
     bool                    mFreeRunning{false};
     android::fifo_counter_t mDataReadCounter{0}; // only used if free-running
     android::fifo_counter_t mDataWriteCounter{0}; // only used if free-running
-
-    std::mutex mDataQueueLock;
 };
 
 } // namespace aaudio
