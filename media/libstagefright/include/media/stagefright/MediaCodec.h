littleGnAl/mirror_platform_frameworks_av

Enabling large audio for BLOCK_MODEL
Bug: 298052174

Change-Id: Iaff87687d7191d2a25e712d9c12c09b8b1abf429

media/libstagefright/include/media/stagefright/MediaCodec.h
@@ -210,8 +210,7 @@ struct MediaCodec : public AHandler {
     status_t queueBuffer(
             size_t index,
             const std::shared_ptr<C2Buffer> &buffer,
-            int64_t presentationTimeUs,
-            uint32_t flags,
+            const sp<RefBase> &infos,
             const sp<AMessage> &tunings,
             AString *errorDetailMsg = NULL);
 
@@ -225,8 +224,7 @@ struct MediaCodec : public AHandler {
             const uint8_t iv[16],
             CryptoPlugin::Mode mode,
             const CryptoPlugin::Pattern &pattern,
-            int64_t presentationTimeUs,
-            uint32_t flags,
+            const sp<RefBase> &infos,
             const sp<AMessage> &tunings,
             AString *errorDetailMsg = NULL);
 

LineageOS/android_frameworks_av

media/libstagefright/include/media/stagefright/MediaCodec.h
@@ -213,8 +213,7 @@ struct MediaCodec : public AHandler {
     status_t queueBuffer(
             size_t index,
             const std::shared_ptr<C2Buffer> &buffer,
-            int64_t presentationTimeUs,
-            uint32_t flags,
+            const sp<BufferInfosWrapper> &bufferInfos,
             const sp<AMessage> &tunings,
             AString *errorDetailMsg = NULL);
 
@@ -228,8 +227,7 @@ struct MediaCodec : public AHandler {
             const uint8_t iv[16],
             CryptoPlugin::Mode mode,
             const CryptoPlugin::Pattern &pattern,
-            int64_t presentationTimeUs,
-            uint32_t flags,
+            const sp<BufferInfosWrapper> &bufferInfos,
             const sp<AMessage> &tunings,
             AString *errorDetailMsg = NULL);
 