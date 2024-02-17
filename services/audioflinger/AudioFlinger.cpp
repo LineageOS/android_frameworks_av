6a458b2879ee7069d34d8bb90a2b6051a941e808
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-02-17 07:26:49 +0530

    audio: Check MODIFY_AUDIO_ROUTING permission for per-app volume
    We need to use this from SystemUI which runs under app uid.
    Instead check for MODIFY_AUDIO_ROUTING permission which is granted only
    to select system apps (including SystemUI) and also matches the API
    @RequiresPermission annotation in framework.
    
    Change-Id: Ib78565c15cbdc8619bf94240a2329e97fd40c6a9

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -5322,6 +5322,22 @@ status_t AudioFlinger::onTransactWrapper(TransactionCode code,
             break;
     }
 
+    // make sure the following transactions require MODIFY_AUDIO_ROUTING permission
+    switch (code) {
+        case TransactionCode::SET_APP_VOLUME:
+        case TransactionCode::SET_APP_MUTE: {
+            if (!modifyAudioRoutingAllowed()) {
+                ALOGW("%s: transaction %d received from PID %d UID %d does not have "
+                      "MODIFY_AUDIO_ROUTING permission",
+                      __func__, code, IPCThreadState::self()->getCallingPid(),
+                      IPCThreadState::self()->getCallingUid());
+                return INVALID_OPERATION;
+            }
+        } break;
+        default:
+            break;
+    }
+
     const std::string methodName = getIAudioFlingerStatistics().getMethodForCode(code);
     mediautils::TimeCheck check(
             std::string("IAudioFlinger::").append(methodName),
