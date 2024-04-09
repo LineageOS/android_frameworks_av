b5fbcfb02b475bb07b55239a23e2cf94e6ea65d5
Harish Mahendrakar <hmahendrakar@google.com>
2024-04-09 14:27:24 -0700

    MPEG4Writer: Remove timestamp validation
    Timestamp validation method that was added doesn't work as expected
    when system time is sent as timestamp to the MPEG4Writer.
    
    Bug: 331723380
    Test: atest CtsMediaMuxerTestCases
    Test: atest CtsMediaV2TestCases:MuxerTest
    Test: atest CtsMediaV2TestCases:MuxerUnitTest
    (cherry picked from https://android-review.googlesource.com/q/commit:f909d513c7bcb4c5e1db6da6440ed3e043b3657f)
    Merged-In: I9cf28cae0c4836035290123ae33cf6d94052585e
    Change-Id: I9cf28cae0c4836035290123ae33cf6d94052585e
    24D1-dev is based on 24Q2-release. Therefore, we merged this CL to 24D1-dev.

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -2436,47 +2436,6 @@ status_t MPEG4Writer::setNextFd(int fd) {
     return OK;
 }
 
-bool MPEG4Writer::isSampleMetadataValid(size_t trackIndex, int64_t timeUs) {
-    // Track Index starts from zero, so it should be at least 1 less than size.
-    if (trackIndex >= mTracks.size()) {
-        ALOGE("Incorrect trackIndex %zu, mTracks->size() %zu", trackIndex, mTracks.size());
-        return false;
-    }
-
-    List<Track *>::iterator it = mTracks.begin();
-
-    // (*it) is already pointing to trackIndex 0.
-    for (int i = 1; i <= trackIndex; i++) {
-        it++;
-    }
-
-    return (*it)->isTimestampValid(timeUs);
-}
-
-bool MPEG4Writer::Track::isTimestampValid(int64_t timeUs) {
-    // No timescale if HEIF
-    if (mIsHeif) {
-        return true;
-    }
-
-    // Make sure abs(timeUs) does not overflow
-    if (timeUs == INT64_MIN) {
-       return false;
-    }
-
-    // Ensure that the timeUs value does not have extremely low or high values
-    // that would cause an underflow or overflow, like in the calculation -
-    // mdhdDuration = (trakDurationUs * mTimeScale + 5E5) / 1E6
-    if (abs(timeUs) >= (INT64_MAX - 5E5) / mTimeScale) {
-        return false;
-    }
-    // Limit check for calculations in ctts box
-    if (abs(timeUs) + kMaxCttsOffsetTimeUs >= INT64_MAX / mTimeScale) {
-        return false;
-    }
-    return true;
-}
-
 bool MPEG4Writer::Track::isExifData(
         MediaBufferBase *buffer, uint32_t *tiffHdrOffset) const {
     if (!mIsHeif) {
