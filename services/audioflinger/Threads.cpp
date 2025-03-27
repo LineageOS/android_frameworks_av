6bf9b791367cf62960031a3c655f8c5c1b888bab
someone5678 <someone5678@users.noreply.github.com>
2025-03-27 01:11:53 -0400

    fixup! av: support per-app volume [1/3]
    * Apply per-app volume to portid_volume_management
    
    Change-Id: I4c7f20ebb7edc2f82a220a7661fcce93d463f5b1

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -5885,10 +5885,11 @@ PlaybackThread::mixer_state MixerThread::prepareTracks_l(
 					      * track->getAppVolume();
                     }
                 } else {
-                    if (track->isPlaybackRestricted() || track->getPortMute()) {
+                    if (track->isPlaybackRestricted() || track->getPortMute() || track->isAppMuted()) {
                         volume = 0.f;
                     } else {
-                        volume = masterVolume * track->getPortVolume();
+                        volume = masterVolume * track->getPortVolume()
+                                              * track->getAppVolume();
                     }
                 }
                 const auto amn = mAfThreadCallback->getAudioManagerNative();
@@ -6090,8 +6091,9 @@ PlaybackThread::mixer_state MixerThread::prepareTracks_l(
                     v = 0;
                 }
             } else {
-                v = masterVolume * track->getPortVolume();
-                if (track->isPlaybackRestricted() || track->getPortMute()) {
+                v = masterVolume * track->getPortVolume()
+                                 * track->getAppVolume();
+                if (track->isPlaybackRestricted() || track->getPortMute() || track->isAppMuted()) {
                     v = 0;
                 }
             }
@@ -6906,11 +6908,12 @@ void DirectOutputThread::processVolume_l(IAfTrack* track, bool lastTrack)
                                    track->isPlaybackRestrictedControl()});
         }
     } else {
-        if (mMasterMute || track->isPlaybackRestricted()) {
+        if (mMasterMute || track->isPlaybackRestricted() || track->isAppMuted()) {
             left = right = 0;
         } else {
             float typeVolume = track->getPortVolume();
-            const float v = mMasterVolume * typeVolume * shaperVolume;
+            float appVolume = track->getAppVolume();
+            const float v = mMasterVolume * typeVolume * shaperVolume * appVolume;
 
             if (left > GAIN_FLOAT_UNITY) {
                 left = GAIN_FLOAT_UNITY;
