36b3139b540df94b042f1b1bbc693fbde8379bfa
Arun Johnson <arunjohnson@google.com>
2025-05-01 10:24:05 -0700

    Remove flag large_audio_frame and large_audio_frame_finish
    Since large_audio_frame is declared is_exported, its
    declaration is not removed yet in aconfig. But all code references are
    removed with this changes, including the CTS.
    
    Bug: 409609657
    Test: Manual
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: I37b0fc0e63b5ffd67f328808fb09772090effd19

--- media/codec2/sfplugin/Codec2InfoBuilder.cpp
+++ media/codec2/sfplugin/Codec2InfoBuilder.cpp
@@ -774,20 +774,17 @@ status_t Codec2InfoBuilder::buildMediaCodecList(MediaCodecListWriter* writer) {
                 addSupportedColorFormats(
                         intf, caps.get(), trait, mediaType, it->second);
 
-                if (com::android::media::codec::flags::provider_->large_audio_frame()
-                        && android::media::codec::provider_->large_audio_frame_finish()) {
-                    // Adding feature-multiple-frames when C2LargeFrame param is present
-                    if (trait.domain == C2Component::DOMAIN_AUDIO) {
-                        std::vector<std::shared_ptr<C2ParamDescriptor>> params;
-                        c2_status_t err = intf->querySupportedParams(&params);
-                        if (err == C2_OK) {
-                            for (const auto &paramDesc : params) {
-                                if (C2LargeFrame::output::PARAM_TYPE == paramDesc->index()) {
-                                    std::string featureMultipleFrames =
-                                            std::string(KEY_FEATURE_) + FEATURE_MultipleFrames;
-                                    caps->addDetail(featureMultipleFrames.c_str(), 0);
-                                    break;
-                                }
+                // Adding feature-multiple-frames when C2LargeFrame param is present
+                if (trait.domain == C2Component::DOMAIN_AUDIO) {
+                    std::vector<std::shared_ptr<C2ParamDescriptor>> params;
+                    c2_status_t err = intf->querySupportedParams(&params);
+                    if (err == C2_OK) {
+                        for (const auto &paramDesc : params) {
+                            if (C2LargeFrame::output::PARAM_TYPE == paramDesc->index()) {
+                                std::string featureMultipleFrames =
+                                        std::string(KEY_FEATURE_) + FEATURE_MultipleFrames;
+                                caps->addDetail(featureMultipleFrames.c_str(), 0);
+                                break;
                             }
                         }
                     }
