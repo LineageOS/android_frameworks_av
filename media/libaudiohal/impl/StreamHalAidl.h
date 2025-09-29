45fd3a0c7d2f9a26c01489bc424561ecd32116fd
Mikhail Naganov <mnaganov@google.com>
2025-09-29 14:39:47 -0700

    Reapply "audio: fix volume unexpectedly set to 0 by VolumeShaper"
    This reverts commit 0c7f603b13239a011d187ec46c6b1cd6b3f8f3c1.
    The revert was due to test failure, which was an issue with the test.
    Relanding the same commit accompanied by the test update.
    
    Original commit description:
    
    Monotonic frame counter reset incorrectly for PCM formats,
    affecting volume calculation.
    Redefine flush/drain/standby behavior to reset position on any type
    of format and on direct output.
    
    Flag: EXEMPT BUGFIX
    Bug: 443044874
    Test: Manual
    Test: atest CtsMediaAudioTestCases
    Change-Id: I4c7ada35e642e32561f3d78b986a9c1e7f8a751e

--- media/libaudiohal/impl/StreamHalAidl.h
+++ media/libaudiohal/impl/StreamHalAidl.h
@@ -50,13 +50,15 @@ class StreamContextAidl {
             ::aidl::android::hardware::common::fmq::SynchronizedReadWrite> DataMQ;
 
     StreamContextAidl(::aidl::android::hardware::audio::core::StreamDescriptor& descriptor,
-                      bool isAsynchronous, int ioHandle, bool hasClipTransitionSupport)
+                      bool isAsynchronous, bool isDirect, int ioHandle,
+                      bool hasClipTransitionSupport)
         : mFrameSizeBytes(descriptor.frameSizeBytes),
           mCommandMQ(new CommandMQ(descriptor.command)),
           mReplyMQ(new ReplyMQ(descriptor.reply)),
           mBufferSizeFrames(descriptor.bufferSizeFrames),
           mDataMQ(maybeCreateDataMQ(descriptor)),
           mIsAsynchronous(isAsynchronous),
+          mIsDirect(isDirect),
           mIsMmapped(isMmapped(descriptor)),
           mMmapBufferDescriptor(maybeGetMmapBuffer(descriptor)),
           mIoHandle(ioHandle),
@@ -84,6 +86,7 @@ class StreamContextAidl {
     size_t getFrameSizeBytes() const { return mFrameSizeBytes; }
     ReplyMQ* getReplyMQ() const { return mReplyMQ.get(); }
     bool isAsynchronous() const { return mIsAsynchronous; }
+    bool isDirect() const { return mIsDirect; }
     bool isMmapped() const { return mIsMmapped; }
     const ::aidl::android::hardware::audio::core::MmapBufferDescriptor&
             getMmapBufferDescriptor() const { return mMmapBufferDescriptor; }
@@ -123,6 +126,7 @@ class StreamContextAidl {
     size_t mBufferSizeFrames;
     std::unique_ptr<DataMQ> mDataMQ;
     bool mIsAsynchronous;
+    bool mIsDirect;
     bool mIsMmapped;
     ::aidl::android::hardware::audio::core::MmapBufferDescriptor mMmapBufferDescriptor;
     int mIoHandle;
