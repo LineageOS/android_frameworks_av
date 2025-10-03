4e85d713f9b4910e1efe6c0bff75f32e4c0ae230
Girish Shetty <girishshetty@google.com>
2025-10-03 21:10:19 +0000

    resourcemanager: implement difference between 2 resource lists
    Bug: 402463766
    Flag: android.media.codec.codec_availability_metrics
    Test: atest android.media.misc.cts.ResourceManagerTest
          atest android.media.misc.cts.ResourceManagerMultiTest
          /data/nativetest64/ResourceManagerService_test/ResourceManagerService_test
          /data/nativetest64/ResourceObserverService_test/ResourceObserverService_test
    Change-Id: I5e3151e03c704c562eb56e1765a93db1fc748e4f

--- services/mediaresourcemanager/ResourceManagerServiceUtils.h
+++ services/mediaresourcemanager/ResourceManagerServiceUtils.h
@@ -264,6 +264,22 @@ ResourceInfo& getResourceInfoForEdit(
 void mergeResources(::aidl::android::media::MediaResourceParcel& r1,
                     const ::aidl::android::media::MediaResourceParcel& r2);
 
+/**
+ * @brief Calculates the difference (delta) between two vectors of resources.
+ *
+ * This function subtracts the amount of each resource in resourcesWhat from the amount
+ * of the matching resource in resourcesFrom.
+ * Resources unique to resourcesFrom retain their amount.
+ * Resources unique to resourcesWhat are included with a negated amount.
+ *
+ * @param resourcesFrom The base vector (Minuend).
+ * @param resourcesWhat The vector to subtract (Subtrahend).
+ * @return std::vector<MediaResourceParcel> The resultant vector containing the difference (Delta).
+ */
+std::vector<::aidl::android::media::MediaResourceParcel> calculateResourceDifference(
+        const std::vector<::aidl::android::media::MediaResourceParcel>& resourcesFrom,
+        const std::vector<::aidl::android::media::MediaResourceParcel>& resourcesWhat);
+
 // To notify the media_resource_monitor about the resource being granted.
 void notifyResourceGranted(
         int pid,
