96e92c33cd9da2abca816feeedd09df4d980e914
Eino-Ville Talvala <etalvala@google.com>
2025-12-01 17:11:10 -0800

    Camera: Fix device type API backcompat bugs
    - Flag the capture result additions
    - Add device type to physical camera metadata as well
    
    Bug: 464276536
    Flag: com.android.internal.camera.flags.camera_device_type_api
    Test: atest CtsCameraTestCases
    Change-Id: I48e34508c070045e4369dcd6006db75d076c290e

--- services/camera/libcameraservice/common/aidl/AidlProviderInfo.cpp
+++ services/camera/libcameraservice/common/aidl/AidlProviderInfo.cpp
@@ -684,12 +684,10 @@ AidlProviderInfo::AidlDeviceInfo3::AidlDeviceInfo3(
         }
     }
 
-    if (flags::camera_device_type_api()) {
-        res = addDeviceTypeTag(mCameraCharacteristics);
-        if (OK != res) {
-            ALOGE("%s: Unable to add device type tag: %s (%d)",
-                __FUNCTION__, strerror(-res), res);
-        }
+    res = addDeviceTypeTag(mCameraCharacteristics);
+    if (OK != res) {
+        ALOGE("%s: Unable to add device type tag: %s (%d)",
+            __FUNCTION__, strerror(-res), res);
     }
 
     camera_metadata_entry flashAvailable =
@@ -798,12 +796,10 @@ AidlProviderInfo::AidlDeviceInfo3::AidlDeviceInfo3(
                 }
             }
 
-            if (flags::camera_device_type_api()) {
-                res = addDeviceTypeTag(mPhysicalCameraCharacteristics[id]);
-                if (OK != res) {
-                    ALOGE("%s: Unable to add device type tag: %s (%d)",
-                        __FUNCTION__, strerror(-res), res);
-                }
+            res = addDeviceTypeTag(mPhysicalCameraCharacteristics[id]);
+            if (OK != res) {
+                ALOGE("%s: Unable to add device type tag: %s (%d)",
+                    __FUNCTION__, strerror(-res), res);
             }
 
         }
