3f37f2fdd897908a5ed5642df7d9636df8167238
chenjiahui <chenjiahui@google.com>
2025-12-15 23:14:58 +0000

    Camera: Add RAW14 link
    Add RAW14 link in the docs
    
    Flag: com.android.internal.camera.flags.camera_raw14_format
    Test: builds
    Bug: 467787351
    Change-Id: I12299e239cf1d52506b8d146e983b7d267b1d39f

--- camera/ndk/include/camera/NdkCameraMetadataTags.h
+++ camera/ndk/include/camera/NdkCameraMetadataTags.h
@@ -3828,8 +3828,9 @@ typedef enum acamera_metadata_tag {
      * <li>Processed (but stalling): any non-RAW format with a stallDurations &gt; 0.
      *   Typically {@link AIMAGE_FORMAT_JPEG JPEG format}.</li>
      * <li>Raw formats: {@link AIMAGE_FORMAT_RAW16 RAW_SENSOR},
-     *   {@link AIMAGE_FORMAT_RAW10 RAW10}, or
-     *   {@link AIMAGE_FORMAT_RAW12 RAW12}.</li>
+     *   {@link AIMAGE_FORMAT_RAW10 RAW10},
+     *   {@link AIMAGE_FORMAT_RAW12 RAW12}, or
+     *   {@link AIMAGE_FORMAT_RAW12 RAW14}</li>
      * <li>Processed (but not-stalling): any non-RAW format without a stall duration.  Typically
      *   {@link AIMAGE_FORMAT_YUV_420_888 YUV_420_888},
      *   <a href="https://developer.android.com/reference/android/graphics/ImageFormat.html#NV21">NV21</a>, <a href="https://developer.android.com/reference/android/graphics/ImageFormat.html#YV12">YV12</a>, or {@link AIMAGE_FORMAT_Y8 Y8} .</li>
@@ -4463,6 +4464,7 @@ typedef enum acamera_metadata_tag {
      * <li>{@link AIMAGE_FORMAT_YUV_420_888 }</li>
      * <li>{@link AIMAGE_FORMAT_RAW10 }</li>
      * <li>{@link AIMAGE_FORMAT_RAW12 }</li>
+     * <li>{@link AIMAGE_FORMAT_RAW14 }</li>
      * <li>{@link AIMAGE_FORMAT_Y8 }</li>
      * </ul>
      * <p>All other formats may or may not have an allowed stall duration on
@@ -10469,7 +10471,7 @@ typedef enum acamera_metadata_enum_acamera_request_available_capabilities {
      * less than 24 megapixels. Also the ACAMERA_CONTROL_AE_AVAILABLE_TARGET_FPS_RANGES entry
      * lists at least one FPS range where the minimum FPS is &gt;= 1 / minimumFrameDuration
      * for the largest YUV_420_888 size.</p>
-     * <p>If the device supports the {@link AIMAGE_FORMAT_RAW10 }, {@link AIMAGE_FORMAT_RAW12 }, {@link AIMAGE_FORMAT_Y8 }, then those can also be
+     * <p>If the device supports the {@link AIMAGE_FORMAT_RAW10 }, {@link AIMAGE_FORMAT_RAW12 }, {@link AIMAGE_FORMAT_RAW14 }, {@link AIMAGE_FORMAT_Y8 }, then those can also be
      * captured at the same rate as the maximum-size YUV_420_888 resolution is.</p>
      * <p>In addition, the ACAMERA_SYNC_MAX_LATENCY field is guaranteed to have a value between 0
      * and 4, inclusive. ACAMERA_CONTROL_AE_LOCK_AVAILABLE and ACAMERA_CONTROL_AWB_LOCK_AVAILABLE
@@ -11270,6 +11272,7 @@ typedef enum acamera_metadata_enum_acamera_scaler_available_stream_use_cases {
      * <li>{@link AIMAGE_FORMAT_RAW16 RAW_SENSOR}</li>
      * <li>{@link AIMAGE_FORMAT_RAW10 RAW10}</li>
      * <li>{@link AIMAGE_FORMAT_RAW12 RAW12}</li>
+     * <li>{@link AIMAGE_FORMAT_RAW14 RAW14}</li>
      * </ul>
      * <p>session configuration is not guaranteed to succeed.</p>
      * <p>This stream use case may not be supported on some devices.</p>
