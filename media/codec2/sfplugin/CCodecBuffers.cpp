4693c1331c7e626ab35a5fd3083073f5d49b8a2e
Bruno Martins <bgcngm@gmail.com>
2026-01-01 10:54:28 +0000

    Revert "codec2: Use numClientBuffers to control the pipeline"
    This reverts commit bfe6cdc5bf3780c5d698ab3a696562e59966e4cb.
    
    No longer present in LA_AU.QSSI.16.1.1.r1-00500-qssi.0.
    
    Change-Id: I9df3d42989547844742b42138c43d981988f5eb7

--- media/codec2/sfplugin/CCodecBuffers.cpp
+++ media/codec2/sfplugin/CCodecBuffers.cpp
@@ -753,14 +753,6 @@ size_t FlexBuffersImpl::numComponentBuffers() const {
             });
 }
 
-size_t FlexBuffersImpl::numClientBuffers() const {
-    return std::count_if(
-            mBuffers.begin(), mBuffers.end(),
-            [](const Entry &entry) {
-                return entry.clientBuffer != nullptr;
-            });
-}
-
 // BuffersArrayImpl
 
 void BuffersArrayImpl::initialize(
@@ -915,14 +907,6 @@ size_t BuffersArrayImpl::arraySize() const {
     return mBuffers.size();
 }
 
-size_t BuffersArrayImpl::numClientBuffers() const {
-    return std::count_if(
-            mBuffers.begin(), mBuffers.end(),
-            [](const Entry &entry) {
-                return entry.ownedByClient;
-            });
-}
-
 // InputBuffersArray
 
 void InputBuffersArray::initialize(
@@ -969,10 +953,6 @@ size_t InputBuffersArray::numActiveSlots() const {
     return mImpl.numActiveSlots();
 }
 
-size_t InputBuffersArray::numClientBuffers() const {
-    return mImpl.numClientBuffers();
-}
-
 sp<Codec2Buffer> InputBuffersArray::createNewBuffer() {
     return mAllocate();
 }
@@ -1009,10 +989,6 @@ std::unique_ptr<InputBuffers> SlotInputBuffers::toArrayMode(size_t) {
 
 size_t SlotInputBuffers::numActiveSlots() const {
     return mImpl.numActiveSlots();
-}
-
-size_t SlotInputBuffers::numClientBuffers() const {
-    return mImpl.numClientBuffers();
 }
 
 sp<Codec2Buffer> SlotInputBuffers::createNewBuffer() {
@@ -1066,10 +1042,6 @@ std::unique_ptr<InputBuffers> LinearInputBuffers::toArrayMode(size_t size) {
 
 size_t LinearInputBuffers::numActiveSlots() const {
     return mImpl.numActiveSlots();
-}
-
-size_t LinearInputBuffers::numClientBuffers() const {
-    return mImpl.numClientBuffers();
 }
 
 // static
@@ -1254,10 +1226,6 @@ size_t GraphicMetadataInputBuffers::numActiveSlots() const {
     return mImpl.numActiveSlots();
 }
 
-size_t GraphicMetadataInputBuffers::numClientBuffers() const {
-    return mImpl.numClientBuffers();
-}
-
 sp<Codec2Buffer> GraphicMetadataInputBuffers::createNewBuffer() {
     std::shared_ptr<C2Allocator> alloc;
     c2_status_t err = mStore->fetchAllocator(mPool->getAllocatorId(), &alloc);
@@ -1336,10 +1304,6 @@ std::unique_ptr<InputBuffers> GraphicInputBuffers::toArrayMode(size_t size) {
 
 size_t GraphicInputBuffers::numActiveSlots() const {
     return mImpl.numActiveSlots();
-}
-
-size_t GraphicInputBuffers::numClientBuffers() const {
-    return mImpl.numClientBuffers();
 }
 
 sp<Codec2Buffer> GraphicInputBuffers::createNewBuffer() {
