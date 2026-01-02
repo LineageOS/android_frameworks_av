c646352578ea58510b0252e69fe189382b0ae083
Sungtak Lee <taklee@google.com>
2026-01-02 22:05:15 +0000

    CCodecBufferChannel: Set dequeueTimeout of surface to zero at start
    CCodec assumes dequeueing from output surface is non-blocking. But the
    output surface given at start is not configured as non-blocking. Ensure
    the output surface given at start is non blocking for dequeueing.
    
    Bug: 466230499
    Flag: EXEMPT BUGFIX
    Test: presubmit & partner
    Change-Id: I006d15da5da02895415aff3eef1089ab8a3bfae3

--- media/codec2/sfplugin/CCodecBufferChannel.cpp
+++ media/codec2/sfplugin/CCodecBufferChannel.cpp
@@ -2143,6 +2143,7 @@ status_t CCodecBufferChannel::start(
                     output->surface->getIGraphicBufferProducer() : nullptr;
             if (outputSurface) {
                 (void)SurfaceCallbackHandler::GetInstance();
+                output->surface->setDequeueTimeout(kDequeueTimeoutNs);
                 output->surface->setMaxDequeuedBufferCount(output->maxDequeueBuffers);
             }
             outputGeneration = output->generation;
