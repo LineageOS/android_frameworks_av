a93e8321f1889a4a6678b5e929927305afa27bb9
Luca Stefani <luca.stefani.ge1@gmail.com>
2026-01-18 13:11:44 +0100

    MediaCodec: Use proper surface when creating PersistentSurface
    It was using the "empty" surface instead of the surface
    created by omx->createInputSurface
    
    Change-Id: Ib7e2a38b42cabaa0bd91a04129d41114ac38afb2

--- media/libstagefright/MediaCodec.cpp
+++ media/libstagefright/MediaCodec.cpp
@@ -1364,7 +1364,7 @@ sp<PersistentSurface> MediaCodec::CreatePersistentInputSurface() {
         return NULL;
     }
 
-    return new PersistentSurface(surface, bufferSource);
+    return new PersistentSurface(mediaflagtools::igbpToSurfaceType(igbp), bufferSource);
 }
 
 /**
