2e0403727a11b34072e9f5e03c05dd049e493194
Andy Hung <hunga@google.com>
2026-03-09 15:41:40 -0700

    audioserver: Update AudioPowerManager on client frozen state
    AudioPowerManager now tracks whether the client has a pid
    that is frozen.  It also internally logs when the freeze
    state occurs with audio activity: the time of last
    occurrence and the number of such occurrences.
    
    Update AudioFlinger to only take action if there is audio activity
    on the pid during frozen state change.
    
    Flag: EXEMPT BUGFIX
    Test: adb shell dumpsys media.audio_flinger
    Bug: 491261114
    Change-Id: I7e5268a008525326ff27a6020bf42436bb7acce2

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -2286,16 +2286,43 @@ void AudioFlinger::NotificationClient::onStateChanged(
 {
     audio_utils::set_priority_for_binder_callback(__func__);
 
+    bool frozen;
+    const char* fstring;
     if (state == IBinder::FrozenStateChangeCallback::State::FROZEN) {
-        mFrozen = true;
-        mAudioFlinger->onClientFrozen(mPid);
+        frozen = true;
+        fstring = "frozen";
     } else if (state == IBinder::FrozenStateChangeCallback::State::UNFROZEN) {
-        mFrozen = false;
-        mAudioFlinger->onClientUnfrozen(mPid);
+        frozen = false;
+        fstring = "unfrozen";
+    } else {
+        ALOGW("%s: unknown state: %d", __func__, state);
+        return;
+    }
+
+    mFrozen = frozen;
+
+    // Querying activeWhileFrozen from AudioPowerManager is lighter weight
+    // than locking the PlaybackThreads if those are active.
+    auto& apm = media::psh_utils::AudioPowerManager::getAudioPowerManager();
+    const bool activeWhileFrozen = apm.setFrozen(mPid, frozen);
+
+    if (activeWhileFrozen) {
+        // We only trigger AudioFlinger frozen state operations if there is legitimate
+        // audio activity for that pid.  This prevents unnecessary locking and unlocking of
+        // Threads which may be intrusive on performance.
+        //
+        // Note: in the future consider mediautils::UidInfo::getInfo(mUid)->package.c_str()
+        ALOGW("%s: pid:%d  uid:%d  new state:%s - active while frozen",
+                __func__, mPid, mUid, fstring);
+        if (frozen) {
+            mAudioFlinger->onClientFrozen(mPid);
+        } else {
+            mAudioFlinger->onClientUnfrozen(mPid);
+        }
+    } else {
+        ALOGD("%s: pid:%d  uid:%d  state:%s - no freeze activity", __func__, mPid, mUid, fstring);
     }
     ALOGV("%s: priority: %d", __func__, audio_utils::get_thread_priority(gettid()));
-    ALOGD("%s: pid:%d  uid:%d  state:%s",
-          __func__, mPid, mUid, (mFrozen ? "frozen" : "unfrozen"));
 }
 
 status_t AudioFlinger::createRecord(const media::CreateRecordRequest& _input,
