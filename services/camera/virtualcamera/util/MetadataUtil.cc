c543cb7212f61c52d801573f72c68013171cccb9
Ján Sebechlebský <jseb@meta.com>
2026-03-23 04:47:30 -0700

    Fix missing null teminator in virtual camera GPS metadata
    ExifUtil relies on null terminator presence (doesn't use metadata size).
    This was causing buffer overrun, and subsequently failure of android.hardware.cts.CameraTest#testJpegExif because of corrupted data in provider string.
    
    Test: atest android.hardware.cts.CameraTest#testJpegExif
    Cherrypick-From: https://android-review.googlesource.com/q/commit:fcaa5e11ef9b5ddf45721408935d497ccb2fefc7
    
    Bug: N/A
    Flag: EXEMPT BUGFIX
    Change-Id: Ie022457b7e413f2690bbc756577f82d9180077c6

--- services/camera/virtualcamera/util/MetadataUtil.cc
+++ services/camera/virtualcamera/util/MetadataUtil.cc
@@ -65,6 +65,15 @@ std::vector<To> asVectorOf(const From from) {
 // Each HAL stream configuration has 4 entries, see metadata_definitions.xml
 constexpr size_t kNumEntriesPerStreamConfiguration = 4;
 
+std::vector<uint8_t> asUint8Vector(const std::string& str) {
+  std::vector<uint8_t> vec;
+  vec.reserve(str.size() + 1);
+  std::copy(str.begin(), str.end(), std::back_inserter(vec));
+  // Add a null terminator.
+  vec.push_back(0);
+  return vec;
+}
+
 }  // namespace
 
 MetadataBuilder& MetadataBuilder::setSupportedHardwareLevel(
@@ -464,8 +473,8 @@ MetadataBuilder& MetadataBuilder::setJpegGpsCoordinates(
                            gpsCoordinates.altitude});
 
   if (!gpsCoordinates.provider.empty()) {
-    mEntryMap[ANDROID_JPEG_GPS_PROCESSING_METHOD] = std::vector<uint8_t>{
-        gpsCoordinates.provider.begin(), gpsCoordinates.provider.end()};
+    mEntryMap[ANDROID_JPEG_GPS_PROCESSING_METHOD] =
+        asUint8Vector(gpsCoordinates.provider);
   }
 
   if (gpsCoordinates.timestamp.has_value()) {
